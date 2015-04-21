a810ace089f37b6f96ce4db4d7eabc3f56e41cc0
Burlen Loring <bloring@lbl.gov>
2015-04-21 14:20:44 -0700

    cf reader documentation

--- io/teca_cf_reader.h
+++ io/teca_cf_reader.h
@@ -12,11 +12,40 @@ class teca_cf_reader;
 using p_teca_cf_reader = std::shared_ptr<teca_cf_reader>;
 using const_p_teca_cf_reader = std::shared_ptr<const teca_cf_reader>;
 
-/// a reader for data stroed in netcdf CF format
+/// a reader for data stored in NetCDF CF format
 /**
+a reader for data stored in NetCDF CF format
+
+reads a set of arrays from  single time step into a cartesian
+mesh. the mesh is optionally subset.
+
 metadata keys:
+    variables - a list of all available variables.
+    <var> -  a metadata object holding all NetCDF attributes for the variable named <var>
+    time variables - a list of all variables with time as the only dimension
+    coordinates - a metadata object holding names and arrays of the coordinate axes
+        x_variable - name of x axis variable
+        y_variable - name of y axis variable
+        z_variable - name of z axis variable
+        t_variable - name of t axis variable
+        x - array of x coordinates
+        y - array of y coordinates
+        z - array of z coordinates
+        t - array of t coordinates
+    files - list of files in this dataset
+    step_count - list of the number of steps in each file
+    number_of_time_steps - total number of time steps in all files
+    whole_extent - index space extent describing (nodal) dimensions of the mesh
 
 request keys:
+    time_step - the time step to read
+    arrays - list of arrays to read
+    extent - index space extents describing the subset of data to read
+
+output:
+    generates a 1,2 or 3D cartesian mesh for the requested timestep
+    on the requested extent with the requested point based arrays
+    and time variables.
 */
 class teca_cf_reader : public teca_algorithm
 {
