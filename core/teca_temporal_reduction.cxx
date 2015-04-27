e20177b2dc4348b0cf2916e9e00393d7e2c76d3a
Burlen Loring <bloring@lbl.gov>
2015-04-27 16:26:46 -0700

    fix set vs insert time_step bug in temporal reduction

--- core/teca_temporal_reduction.cxx
+++ core/teca_temporal_reduction.cxx
@@ -122,7 +122,7 @@ std::vector<teca_metadata> teca_temporal_reduction::get_upstream_request(
         for (size_t j = 0; j < n_reqs; ++j)
         {
             up_req.push_back(base_req[j]);
-            up_req.back().set("time_step", ii);
+            up_req.back().insert("time_step", ii);
         }
     }
 
