38338bfe55da59fa300429773911f2c7ae1e77df
Burlen Loring <bloring@lbl.gov>
2015-12-03 16:24:13 -0800

    shotgun dependency tracking for swig bindings

--- CMake/teca_python.cmake
+++ CMake/teca_python.cmake
@@ -1,8 +1,10 @@
-function(wrap_swig input output depends)
+function(wrap_swig input output)
     if (swig_cmd)
+        # TODO -- I shouldn't have to do this...
+        file(GLOB depends ${CMAKE_CURRENT_SOURCE_DIR} *.h *.i)
         add_custom_command(
             OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${output}
-            COMMAND ${swig_cmd} -c++ -python -w341
+            COMMAND ${swig_cmd} -c++ -python -w341 -DSWIG_TYPE_TABLE=teca_py
                 -I${CMAKE_CURRENT_SOURCE_DIR}/../core
                 -I${CMAKE_CURRENT_SOURCE_DIR}/../data
                 -I${CMAKE_CURRENT_SOURCE_DIR}/../io
@@ -13,10 +15,10 @@ function(wrap_swig input output depends)
             COMMENT "wrapping ${input}...")
     endif()
 endfunction()
-function(teca_python_module mname deps)
+function(teca_python_module mname)
     include_directories(SYSTEM
         ${PYTHON_INCLUDE_PATH} ${NUMPY_INCLUDE_DIR})
-    wrap_swig(teca_py_${mname}.i teca_py_${mname}.cxx ${deps})
+    wrap_swig(teca_py_${mname}.i teca_py_${mname}.cxx)
     PYTHON_ADD_MODULE(_teca_py_${mname}
         ${CMAKE_CURRENT_SOURCE_DIR}/teca_py_${mname}.cxx)
     target_link_libraries(_teca_py_${mname} ${PYTHON_LIBRARIES} teca_${mname})
