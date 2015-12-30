72364613bba2eda22de9c1e0cef1334018964f2a
Burlen Loring <bloring@lbl.gov>
2015-12-30 12:09:18 -0800

    version description
    add configuration define for the git tag/sha
    to be used in error messages and output files
    for provenance

--- core/teca_common.h
+++ core/teca_common.h
@@ -1,12 +1,14 @@
 #ifndef teca_common_h
 #define teca_common_h
 
+#include "teca_config.h"
 #include "teca_parallel_id.h"
 #include <iostream>
 
-#define TECA_ERROR(msg)                                          \
-    std::cerr << teca_parallel_id()                              \
-        << " ERROR " << __FILE__ << ":" << __LINE__ << std::endl \
+#define TECA_ERROR(msg)                                             \
+    std::cerr << teca_parallel_id()                                 \
+        << " ERROR " << __FILE__ << ":" << __LINE__ << std::endl    \
+        << TECA_VERSION_DESCR << std::endl                          \
         << "" msg << std::endl;
 
 #endif
