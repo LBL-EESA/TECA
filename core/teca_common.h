6a931120e4f1980f5f3579c80c92d0487e48d0dd
Burlen Loring <bloring@lbl.gov>
2015-12-30 13:57:18 -0800

    better error message
    add version info and ansi color codes

--- core/teca_common.h
+++ core/teca_common.h
@@ -5,10 +5,14 @@
 #include "teca_parallel_id.h"
 #include <iostream>
 
-#define TECA_ERROR(msg)                                             \
-    std::cerr << teca_parallel_id()                                 \
-        << " ERROR " << __FILE__ << ":" << __LINE__ << std::endl    \
-        << TECA_VERSION_DESCR << std::endl                          \
-        << "" msg << std::endl;
+#define TECA_ERROR(msg)                                 \
+    std::cerr                                           \
+        << "\033[1;31mERROR:\033[0m "                   \
+        << teca_parallel_id()                           \
+        << " [" << __FILE__ << ":" << __LINE__          \
+        << " " << TECA_VERSION_DESCR                    \
+        << "]" << std::endl                             \
+        << "\033[1;31mERROR:\033[0m "                   \
+        << "\033[1;37m" msg << "\033[0m" << std::endl;
 
 #endif
