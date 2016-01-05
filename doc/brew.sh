f649d73fdd6f9dd927470406e2dc8a2a11bb37d6
Burlen Loring <bloring@lbl.gov>
2016-01-05 12:28:04 -0800

    refine brew dependency install script

--- doc/brew.sh
+++ doc/brew.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# if you are having issues then unistall everything and start from
-# scratch
-#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-# you may need to clean up /usr/local/
-# you may need to prepend /usr/local/bin to the PATH in ~/.bashrc
+# first install homebrew if you don't have it
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# install homebrew
-#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# now time to fix any and all error reports
+# especially those relating to command line
+# tools and Python
+brew doctor
 
-# some basics
-#brew install gcc
-#brew install gdb
+# install some basics
+brew install gcc
+brew install gdb
 
-# here are teca dependencies
+# install TECA's dependencies
 brew install cmake
 brew install python
 brew install swig
@@ -23,3 +22,11 @@ brew tap homebrew/science
 brew install netcdf
 brew install boost --c++11
 #brew install vtk
+
+# if you are having issues at this point
+# then unistall everything and start from
+# scratch
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+# you may need to clean up /usr/local/
+# you may need to prepend /usr/local/bin to the PATH in ~/.bashrc
+
