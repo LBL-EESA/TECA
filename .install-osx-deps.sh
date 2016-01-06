5c43ebccc8363586a2a401e88f77a55deff5738b
Jeffrey Johnson <jnjohnson@lbl.gov>
2016-01-06 15:14:26 -0800

    Eliminating *_DIR from install dependencies.

--- .install-osx-deps.sh
+++ .install-osx-deps.sh
@@ -3,5 +3,3 @@ brew tap Homebrew/homebrew-science
 brew install gcc openmpi hdf5 netcdf python swig
 pip install numpy
 export PATH=/usr/local/bin:$PATH
-export NETCDF_DIR=/usr/local
-export BOOST_DIR=/usr/local
