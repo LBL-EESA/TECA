47392fedfb0f965844a6ad8953f22241474ead80
Burlen Loring <bloring@lbl.gov>
2016-01-06 12:16:15 -0800

    command line options for connected components test

--- test/test_connected_components.cpp
+++ test/test_connected_components.cpp
@@ -12,78 +12,61 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    if (argc != 12)
     {
-        cerr << "test_connected_components [bool 3d] [dataset regex]" << endl;
+        cerr << "test_connected_components [dataset regex] "
+            "[x var] [y var] [z var] [t var] [u var] [v var] "
+            "[first step] [last step] [threshold] [out file]"
+            << endl;
         return -1;
     }
 
-    int use_3d = atoi(argv[1]);
-    string dataset_regex = argv[2];
+    string dataset_regex = argv[1];
+    string x_var = argv[2];
+    string y_var = argv[3];
+    string z_var = argv[4];
+    string t_var = argv[5];
+    string u_var = argv[6];
+    string v_var = argv[7];
+    int first_step = atoi(argv[8]);
+    int last_step = atoi(argv[9]);
+    double threshold = atof(argv[10]);
+    string out_file = argv[11];
 
     p_teca_cf_reader cfr = teca_cf_reader::New();
-    if (use_3d)
-    {
-        cfr->set_files_regex(dataset_regex);
-        cfr->set_x_axis_variable("lon");
-        cfr->set_y_axis_variable("lat");
-        cfr->set_z_axis_variable("plev");
-        cfr->set_t_axis_variable("time");
-    }
-    else
-    {
-        cfr->set_files_regex(dataset_regex);
-        cfr->set_x_axis_variable("lon");
-        cfr->set_y_axis_variable("lat");
-        cfr->set_t_axis_variable("time");
-    }
+    cfr->set_files_regex(dataset_regex);
+    cfr->set_x_axis_variable(x_var);
+    cfr->set_y_axis_variable(y_var);
+    cfr->set_z_axis_variable(z_var);
+    cfr->set_t_axis_variable(t_var);
 
     p_teca_mask mask = teca_mask::New();
     mask->set_low_threshold_value(1e4);
     mask->set_mask_value(0);
-    mask->append_mask_variable("U");
-    mask->append_mask_variable("V");
+    mask->append_mask_variable(u_var);
+    mask->append_mask_variable(v_var);
     mask->set_input_connection(cfr->get_output_port());
 
     p_teca_l2_norm l2n = teca_l2_norm::New();
-    if (use_3d)
-    {
-        l2n->set_component_0_variable("U");
-        l2n->set_component_1_variable("V");
-    }
-    else
-    {
-        l2n->set_component_0_variable("U850");
-        l2n->set_component_1_variable("V850");
-    }
+    l2n->set_component_0_variable(u_var);
+    l2n->set_component_1_variable(v_var);
     l2n->set_l2_norm_variable("wind_speed");
     l2n->set_input_connection(mask->get_output_port());
 
     p_teca_connected_components cc = teca_connected_components::New();
     cc->set_threshold_variable("wind_speed");
-    if (use_3d)
-    {
-        cc->set_low_threshold_value(30);
-        cc->set_label_variable("30_mps_ccomps");
-    }
-    else
-    {
-        cc->set_low_threshold_value(15);
-        cc->set_label_variable("15_mps_ccomps");
-    }
+    cc->set_low_threshold_value(threshold);
+    cc->set_label_variable("con_comps");
     cc->set_input_connection(l2n->get_output_port());
 
     p_teca_time_step_executive exe = teca_time_step_executive::New();
-    exe->set_first_step(0);
-    exe->set_last_step(0);
+    exe->set_first_step(first_step);
+    exe->set_last_step(last_step);
 
     p_teca_vtk_cartesian_mesh_writer wri = teca_vtk_cartesian_mesh_writer::New();
     wri->set_input_connection(cc->get_output_port());
     wri->set_executive(exe);
-    if (use_3d)
-        wri->set_file_name("3d_ccomps_%t%.vtk");
-    else
-        wri->set_file_name("2d_ccomps_%t%.vtk");
+    wri->set_file_name(out_file);
 
     wri->update();
 
