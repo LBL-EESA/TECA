884df8bbfd8cccefce75bfd3e61daec0faa06701
Burlen Loring <bloring@lbl.gov>
2016-08-30 12:21:13 -0700

    Python extension CMake infrastructure

---
+++ CMake/teca_python.cmake
@@ -0,0 +1,37 @@
+function(teca_py_install)
+    if (TECA_HAS_PYTHON)
+        set(pysrcs ${ARGV})
+        list(GET pysrcs 0 dest)
+        list(REMOVE_AT pysrcs 0)
+        # pass the list of python based applications
+        # this function copies the files into the build binary dir,
+        # sets up the install, and py-compiles the sources
+        foreach(pysrc ${pysrcs})
+            configure_file(${pysrc}
+                ${CMAKE_CURRENT_BINARY_DIR}/../${dest}/${pysrc}
+                COPYONLY)
+        endforeach()
+        install(FILES ${pysrcs} DESTINATION ${dest})
+        # TODO compile the sources
+    endif()
+endfunction()
+
+function(teca_py_install_apps)
+    if (TECA_HAS_PYTHON)
+        # pass the list of python based applications
+        # this function copies the files into the build binary dir,
+        # sets up the install, and py-compiles the sources
+        set(pysrcs ${ARGV})
+        set(pyapps)
+        foreach(pysrc ${pysrcs})
+            get_filename_component(appname ${pysrc} NAME_WE)
+            configure_file(${pysrc}
+                ${CMAKE_CURRENT_BINARY_DIR}/../bin/${appname}
+                COPYONLY)
+            list(APPEND pyapps
+                ${CMAKE_CURRENT_BINARY_DIR}/../bin/${appname})
+        endforeach()
+        install(PROGRAMS ${pyapps} DESTINATION bin)
+        # TODO compile the sources
+    endif()
+endfunction()
