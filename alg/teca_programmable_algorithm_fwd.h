fea537044b49e9dd0b3ccd1779edbeaf265ab892
Burlen Loring <bloring@lbl.gov>
2016-11-28 16:26:22 -0800

    teca_programmable_algorithm_fwd missing includes

--- alg/teca_programmable_algorithm_fwd.h
+++ alg/teca_programmable_algorithm_fwd.h
@@ -2,6 +2,8 @@
 #define teca_program_algorithm_fwd_h
 
 #include "teca_shared_object.h"
+#include "teca_metadata.h"
+#include "teca_dataset_fwd.h"
 
 TECA_SHARED_OBJECT_FORWARD_DECL(teca_programmable_algorithm)
 
@@ -10,18 +12,14 @@ typedef void* report_callback_t;
 typedef void* request_callback_t;
 typedef void* execute_callback_t;
 #else
-using report_callback_t
-    = std::function<teca_metadata(
+using report_callback_t = std::function<teca_metadata(
         unsigned int, const std::vector<teca_metadata>&)>;
 
-using request_callback_t
-    = std::function<std::vector<teca_metadata>(
-        unsigned int,
-        const std::vector<teca_metadata> &,
+using request_callback_t = std::function<std::vector<teca_metadata>(
+        unsigned int, const std::vector<teca_metadata> &,
         const teca_metadata &)>;
 
-using execute_callback_t
-    = std::function<const_p_teca_dataset(
+using execute_callback_t = std::function<const_p_teca_dataset(
         unsigned int, const std::vector<const_p_teca_dataset> &,
         const teca_metadata &)>;
 #endif
