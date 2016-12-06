aa61360dd65ba17c396c16fa3b9a19cd4327e06f
Burlen Loring <bloring@lbl.gov>
2016-12-06 13:52:09 -0800

    link zlib with libxlsxwriter

--- CMake/FindLibXLSXWriter.cmake
+++ CMake/FindLibXLSXWriter.cmake
@@ -5,23 +5,30 @@
 #  LIBXLSXWRITER_LIBRARY      - user modifiable choice of where netcdf libraries are
 
 #search starting from user editable cache var
-if (LIBXLSXWRITER_INCLUDE_DIR AND LIBXLSXWRITER_LIBRARY)
+if(LIBXLSXWRITER_INCLUDE_DIR AND LIBXLSXWRITER_LIBRARY)
   # Already in cache, be silent
-  set (LIBXLSXWRITER_FIND_QUIETLY TRUE)
-endif ()
+  set(LIBXLSXWRITER_FIND_QUIETLY TRUE)
+endif()
 
 set(USE_DEFAULT_PATHS "NO_DEFAULT_PATH")
 if(LIBXLSXWRITER_USE_DEFAULT_PATHS)
   set(USE_DEFAULT_PATHS "")
 endif()
 
-find_path (LIBXLSXWRITER_INCLUDE_DIR xlsxwriter.h
+find_path(LIBXLSXWRITER_INCLUDE_DIR xlsxwriter.h
   HINTS "${LIBXLSXWRITER_DIR}/include")
-mark_as_advanced (LIBXLSXWRITER_INCLUDE_DIR)
+mark_as_advanced(LIBXLSXWRITER_INCLUDE_DIR)
 
-find_library (LIBXLSXWRITER_LIBRARY NAMES xlsxwriter
+find_library(LIBXLSXWRITER_LIBRARY NAMES xlsxwriter
   HINTS "${LIBXLSXWRITER_DIR}/lib")
-mark_as_advanced (LIBXLSXWRITER_LIBRARY)
+mark_as_advanced(LIBXLSXWRITER_LIBRARY)
+
+find_package(ZLIB REQUIRED)
+
+set(LIBXLSXWRITER_LIBRARIES
+    ${LIBXLSXWRITER_LIBRARY} ${ZLIB_LIBRARIES} CACHE STRING
+    "link time dpendencies of the libxlswriter library")
+mark_as_advanced(LIBXLSXWRITER_LIBRARIES)
 
 # handle the QUIETLY and REQUIRED arguments and set LIBXLSXWRITER_FOUND to TRUE if
 # all listed variables are TRUE
