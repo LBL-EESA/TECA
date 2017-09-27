e75d1105c70805a4693262d26f1a9c1eb9b56394
Burlen Loring <bloring@lbl.gov>
2017-09-27 15:42:33 -0700

    remove me -- default actions for ar template

--- alg/teca_ar_template.py
+++ alg/teca_ar_template.py
@@ -28,7 +28,6 @@ class teca_sample_track(object):
         return teca_sample_track()
 
     def __init__(self):
-
         self.impl = teca_py.teca_programmable_algorithm.New()
         self.impl.set_number_of_input_connections(1)
         self.impl.set_number_of_output_ports(1)
@@ -47,19 +46,19 @@ class teca_sample_track(object):
 
     def get_report_callback(self):
         def report_callback(port, md_in):
-
-            return md_out
+            # pass through by default
+            return md_in
         return report_callback
 
     def get_request_callback(self):
         def request_callback(port, md_in, req_in):
-
+            # pass the request through
+            reqs_out = [req_in]
             return reqs_out
         return request_callback
 
     def get_execute_callback(self):
         def execute_callback(port, data_in, req_in):
-
             # return an empty table
             return teca_table.New()
         return execute_callback
