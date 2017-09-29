d8e71e89261dee77bdb0b77b4f5f5580af34654a
Burlen Loring <bloring@lbl.gov>
2017-09-29 14:48:42 -0700

    WIP -- see if we can work around time out in Travis CI Apple

--- test/travis_ci/install_osx.sh
+++ test/travis_ci/install_osx.sh
@@ -15,4 +15,16 @@ wget https://cmake.org/files/v3.5/cmake-3.5.2-Darwin-x86_64.tar.gz
 tar -C /usr/local -x -z -f cmake-3.5.2-Darwin-x86_64.tar.gz --strip-components=3
 
 # install data files.
-travis_wait svn co svn://missmarple.lbl.gov/work3/teca/TECA_data
+# On Apple svn is very very slow. On my mac book pro
+# this command takes over 14 minutes, while on a Linux
+# system on the same network it takes less than 2 minutes.
+# travis will kill a build  if it does not get console output
+# for 10 min. The following snippet sends progress marks
+# to the console while svn runs.
+svn co svn://missmarple.lbl.gov/work3/teca/TECA_data &
+svn_pid=$!
+while [ -n "$(ps -p $svn_pid -o pid=)" ]
+do
+  echo -n "."
+  sleep 2s
+done
