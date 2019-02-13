72cbfda85499ee6f5aad2f4e74a71f7d3eb7a8ba
Abdelrahman Elbashandy <aaelbashandy@lbl.gov>
2019-02-13 14:23:12 -0800

    Implemented the damper algorithm
    add api for inserting variant arrays into metadata
    
    fix typo in default coordinate axes names in the cf reader
    
    connected components output component labels in dataset metadata
    
    fix strncpy off by 1 compiler warnings in calcalcs
    
    add teca_2d_component_area
    
    this calculates area of labeld regions. regions are identified
    by a unique integer value assigned to each point in the mesh
    that belongs in the region. the area is caclulated in spherical
    lat lon coordinate system
    
    add numerical validation to connected components regression test
    
    Fixed the damper algorithm -- Now it applies the inverted gaussian filter on the input latitude wise
    
    Cleaned up the code as required to match the conventional coding style
    
    Made changes to the regression test
    
    squash me -- shared casting w/ abdel
    
    Added an input that represents HWHM so we can calculate sigma from it
    
    Added the python bindings for the new class
    
    Removed the unused variable that was causing the warning on TravisCI

---
+++ test/test_damper.cpp
@@ -0,0 +1,126 @@
+#include "teca_cf_reader.h"
+#include "teca_damper.h"
+#include "teca_l2_norm.h"
+#include "teca_connected_components.h"
+#include "teca_vtk_cartesian_mesh_writer.h"
+#include "teca_time_step_executive.h"
+#include "teca_system_interface.h"
+#include "teca_dataset_capture.h"
+#include "teca_metadata.h"
+#include "teca_dataset.h"
+#include "teca_cartesian_mesh.h"
+#include "teca_variant_array.h"
+#include "teca_dataset_source.h"
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+using prop_map_t = std::map<std::string, p_teca_variant_array>;
+
+int main(int argc, char **argv)
+{
+    teca_system_interface::set_stack_trace_on_error();
+
+    // ======================================================
+    if (argc != 5)
+    {
+        cerr << "test_damper [nx] [ny] [hwhm] [out file]" << endl;
+        return -1;
+    }
+
+    unsigned long nx = atoi(argv[1]);
+    unsigned long ny = atoi(argv[2]);
+    double hwhm = double(atoi(argv[3]));
+    string out_file = argv[4];
+
+    // allocate a mesh
+    // coordinate axes
+    using coord_t = double;
+    coord_t dx = coord_t(360.)/coord_t(nx - 1);
+    p_teca_variant_array_impl<coord_t> x = teca_variant_array_impl<coord_t>::New(nx);
+    coord_t *px = x->get();
+    for (unsigned long i = 0; i < nx; ++i)
+        px[i] = i*dx;
+
+    coord_t dy = coord_t(180.)/coord_t(ny - 1);
+    p_teca_variant_array_impl<coord_t> y = teca_variant_array_impl<coord_t>::New(ny);
+    coord_t *py = y->get();
+    for (unsigned long i = 0; i < ny; ++i)
+        py[i] = coord_t(-90.) + i*dy;
+
+    p_teca_variant_array_impl<coord_t> z = teca_variant_array_impl<coord_t>::New(1);
+    z->set(0, 0.f);
+
+    p_teca_variant_array_impl<coord_t> t = teca_variant_array_impl<coord_t>::New(1);
+    t->set(0, 1.f);
+
+    unsigned long nxy = nx * ny;
+    p_teca_double_array ones_grid = teca_double_array::New(nxy);
+    double *p_ones_grid = ones_grid->get();
+
+    for (unsigned int i = 0; i < nxy; ++i)
+    {
+        p_ones_grid[i] = 1;
+    }
+
+    // Calculating sigma = hwhm/sqrt(2*ln(2))
+    double sigma = hwhm / 1.17741;
+    //cerr << "sigma: " << sigma << endl;
+
+    unsigned long wext[] = {0, nx - 1, 0, ny - 1, 0, 0};
+
+    p_teca_cartesian_mesh mesh = teca_cartesian_mesh::New();
+    mesh->set_x_coordinates(x);
+    mesh->set_y_coordinates(y);
+    mesh->set_z_coordinates(z);
+    mesh->set_whole_extent(wext);
+    mesh->set_extent(wext);
+    mesh->set_time(1.0);
+    mesh->set_time_step(0ul);
+    mesh->get_point_arrays()->append("ones_grid", ones_grid);
+
+    teca_metadata md;
+    md.insert("whole_extent", wext, 6);
+    md.insert("time_steps", std::vector<unsigned long>({0}));
+    md.insert("variables", std::vector<std::string>({"ones_grid"}));
+    md.insert("number_of_time_steps", 1);
+
+    // build the pipeline
+    p_teca_dataset_source source = teca_dataset_source::New();
+    source->set_metadata(md);
+    source->set_dataset(mesh);
+
+    p_teca_damper damped_comp = teca_damper::New();
+    damped_comp->set_filter_lat_width_value(sigma);
+    damped_comp->append_damper_variable("ones_grid");
+    damped_comp->set_input_connection(source->get_output_port());
+
+    p_teca_dataset_capture damp_o = teca_dataset_capture::New();
+    damp_o->set_input_connection(damped_comp->get_output_port());
+
+    p_teca_time_step_executive exe = teca_time_step_executive::New();
+    exe->set_first_step(0);
+    exe->set_last_step(0);
+
+    p_teca_vtk_cartesian_mesh_writer wri = teca_vtk_cartesian_mesh_writer::New();
+    wri->set_input_connection(damp_o->get_output_port());
+    wri->set_executive(exe);
+    wri->set_file_name(out_file);
+
+    wri->update();
+
+    const_p_teca_dataset ds = damp_o->get_dataset();
+
+    //const_p_teca_cartesian_mesh cds = std::dynamic_pointer_cast<const teca_cartesian_mesh>(ds);
+
+    //const_p_teca_variant_array va = cds->get_point_arrays()->get("ones_grid_damped");
+    //cerr << "test_damper -- damped output:" << endl;
+    //va->to_stream(cerr);
+    //cerr << endl;
+
+
+    return 0;
+}
