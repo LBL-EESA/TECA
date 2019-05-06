d5e2bc466f5cdfaaa9b7c8a36e34b3d076815f6f
Burlen Loring <bloring@lbl.gov>
2019-05-06 16:20:00 -0700

    support negative testing
    Tests relying on detecting an error to pass can now be enabled
    by passing `WILL_FAIL` flag to teca_add_test. This will cause
    ctest to pass when `ERROR` is found in the test output. See
    test_dataset_diff, for an example which can be run in positive
    (verifies 2 datasets are the same) or negative(verifies 2 datasets
    are different)  mode.

--- test/test_dataset_diff.cpp
+++ test/test_dataset_diff.cpp
@@ -51,71 +51,58 @@ struct execute_create_test_table
     }
 };
 
-void write_test_tables(const string& file_name, long num_tables)
+int main(int argc, char **argv)
 {
+    teca_system_interface::set_stack_trace_on_error();
+
+    if (argc < 2)
+    {
+        std::cerr << "Error: test_dataset_diff [compare same]" << std::endl;
+        return -1;
+    }
+
+    int compare_same = atoi(argv[1]);
+
+    // write 2 test files.
     p_teca_programmable_algorithm s = teca_programmable_algorithm::New();
     s->set_number_of_input_connections(0);
     s->set_number_of_output_ports(1);
-    s->set_report_callback(report(num_tables));
+    s->set_report_callback(report(2));
     s->set_execute_callback(execute_create_test_table());
 
     p_teca_table_writer w = teca_table_writer::New();
     w->set_input_connection(s->get_output_port());
     w->set_executive(teca_index_executive::New());
-    w->set_file_name(file_name);
+    w->set_file_name("dataset_diff_test_%t%.%e%");
     w->set_output_format_bin();
 
     w->update();
-}
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
 
-    teca_system_interface::set_stack_trace_on_error();
+    // read two file and compare them. if compare_same is true then
+    // read the first file on both readers, if not read both files
+    p_teca_table_reader r1 = teca_table_reader::New();
+    p_teca_table_reader r2 = teca_table_reader::New();
 
-    // Write 2 test files.
-    write_test_tables("dataset_diff_test_%t%.%e%", 2);
-
+    if (compare_same)
     {
-      // Set up a pair of readers that read a single file. The resulting
-      // datasets should be identical.
-      p_teca_table_reader r1 = teca_table_reader::New();
-      r1->set_file_name("dataset_diff_test_0.bin");
-
-      p_teca_table_reader r2 = teca_table_reader::New();
-      r2->set_file_name("dataset_diff_test_0.bin");
-
-      // Set up the dataset diff algorithm to accept the two readers.
-      p_teca_dataset_diff diff = teca_dataset_diff::New();
-      diff->set_input_connection(0, r1->get_output_port());
-      diff->set_input_connection(1, r2->get_output_port());
-
-      diff->update();
+        // these datasets are the same, hence diff should pass
+        r1->set_file_name("dataset_diff_test_0.bin");
+        r2->set_file_name("dataset_diff_test_0.bin");
+    }
+    else
+    {
+        // these datasets are different, hence diff should fail
+        r1->set_file_name("dataset_diff_test_0.bin");
+        r2->set_file_name("dataset_diff_test_1.bin");
     }
 
-    // TODO -- disabled because the word ERROR in the test output causes the test
-    // to fail, but in this case we expect it to fail, and that would actually
-    // be success. Our cmake/ctest code does not yet handle that type of test
-    // The following is a test of the dataset_diff algorithms ability to
-    // correctly identify two different datasets.
-    //
-    //{
-    //    // Set up a pair of readers that read different files. The resulting
-    //    // datasets should be different.
-    //    p_teca_table_reader r1 = teca_table_reader::New();
-    //    r1->set_file_name("dataset_diff_test_0.bin");
-    //    p_teca_table_reader r2 = teca_table_reader::New();
-    //    r2->set_file_name("dataset_diff_test_1.bin");
-    //
-    //    // Set up the dataset diff algorithm to accept the two readers.
-    //    p_teca_dataset_diff diff = teca_dataset_diff::New();
-    //    diff->set_input_connection(0, r1->get_output_port());
-    //    diff->set_input_connection(1, r2->get_output_port());
-    //
-    //    diff->update();
-    //}
+    // diff the output of the readers
+    p_teca_dataset_diff diff = teca_dataset_diff::New();
+    diff->set_input_connection(0, r1->get_output_port());
+    diff->set_input_connection(1, r2->get_output_port());
+
+    diff->update();
 
     return 0;
 }
