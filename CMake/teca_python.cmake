LBL-EESA/TECA

commit 85d8d9337f903f8f59b7cb49e95e71529c09596c

Closes #219 Python version is specified in the shebang.

CMake/teca_python.cmake
@@ -26,8 +26,8 @@ function(teca_py_install_apps)
         foreach(pysrc ${pysrcs})
             get_filename_component(appname ${pysrc} NAME_WE)
             configure_file(${pysrc}
-                ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname}
-                COPYONLY)
+                ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname}.py
+                @ONLY)
             list(APPEND pyapps
                 ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname})
         endforeach()

apps/CMakeLists.txt
@@ -28,10 +28,10 @@ teca_add_app(teca_metadata_probe LIBS ${teca_app_link}
     FEATURES (TECA_HAS_BOOST AND TECA_HAS_NETCDF AND TECA_HAS_UDUNITS))
 
 teca_py_install_apps(
-    teca_convert_table.py
-    teca_dataset_metadata.py
-    teca_event_filter.py
-    teca_tc_stats.py
-    teca_tc_wind_radii_stats.py
-    teca_tc_trajectory_scalars.py
+    teca_convert_table.py.in
+    teca_dataset_metadata.py.in
+    teca_event_filter.py.in
+    teca_tc_stats.py.in
+    teca_tc_wind_radii_stats.py.in
+    teca_tc_trajectory_scalars.py.in
     )

apps/teca_convert_table.py.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python@TECA_PYTHON_VERSION@
 from teca import *
 import sys
 import argparse

apps/teca_dataset_metadata.py.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python@TECA_PYTHON_VERSION@
 from teca import *
 import sys
 

apps/teca_event_filter.py.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python@TECA_PYTHON_VERSION@
 from teca import *
 import sys
 import argparse

apps/teca_tc_stats.py.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python@TECA_PYTHON_VERSION@
 import sys
 from teca import *
 import argparse

apps/teca_tc_trajectory_scalars.py.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python@TECA_PYTHON_VERSION@
 try:
     from mpi4py import *
     rank = MPI.COMM_WORLD.Get_rank()

apps/teca_tc_wind_radii_stats.py.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python@TECA_PYTHON_VERSION@
 import sys
 from teca import *
 import argparse

commit 02f7d7b0629f9d89b28da939e40727109ee4e385

revert Python app name changes
This fixes bugs introduced in 85d8d9337 & 748ded0cac where Python applications
were incorrectly renamed during configuration.

CMake/teca_python.cmake
@@ -26,10 +26,10 @@ function(teca_py_install_apps)
         foreach(pysrc ${pysrcs})
             get_filename_component(appname ${pysrc} NAME_WE)
             configure_file(${pysrc}
-                ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname}.py
+                ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname}
                 @ONLY)
             list(APPEND pyapps
-                ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname}.py)
+                ${CMAKE_CURRENT_BINARY_DIR}/../${BIN_PREFIX}/${appname})
         endforeach()
         install(PROGRAMS ${pyapps} DESTINATION ${BIN_PREFIX})
         # TODO compile the sources