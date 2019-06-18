a0d8b1ee45b7900e6821e173e8f3510eee9796e2
Travis O'Brien <TAObrien@lbl.gov>
2019-06-18 10:58:10 -0700

    Added parameters from multi-person, combined MCMC run

--- apps/teca_bayesian_ar_detector_parameters.h
+++
@@ -1,6 +0,0 @@
-using parameter_t = double;
-const size_t num_ar_detector_parameters = 2;
-
-parameter_t quantile_array         [num_ar_detector_parameters] = {80., 81.};
-parameter_t filter_lat_width_array [num_ar_detector_parameters] = {15, 16.};
-parameter_t min_area_kmsq_array    [num_ar_detector_parameters] = {1e6, 1.5e6};
