fe693d7de2f4369eaeed0b0b8269a91408e04a86
Burlen Loring <bloring@lbl.gov>
2019-09-27 11:05:14 -0700

    add a test for tc_wind_radii_stats algorithm

---
+++ test/python/test_tc_wind_radii_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+import os
+from teca import *
+
+if len(sys.argv) != 3:
+    sys.stderr.write('ERROR:\ntest_tc_wind_radii_stats.py [input] [baseline]\n\n')
+    sys.exit(-1)
+
+infile = sys.argv[1]
+baseline = sys.argv[2]
+
+reader = teca_table_reader.New()
+reader.set_file_name(infile)
+
+#cal = teca_table_calendar.New()
+#cal.set_input_connection(reader.get_output_port())
+#cal.set_time_column('start_time')
+
+stats = teca_tc_wind_radii_stats.New()
+stats.set_input_connection(reader.get_output_port())
+
+if os.path.exists(baseline):
+    table_reader = teca_table_reader.New()
+    table_reader.set_file_name(baseline)
+    diff = teca_dataset_diff.New()
+    diff.set_input_connection(0, table_reader.get_output_port())
+    diff.set_input_connection(1, stats.get_output_port())
+    diff.update()
+else:
+    sys.stderr.write('generating baseline\n')
+    table_writer = teca_table_writer.New()
+    table_writer.set_input_connection(stats.get_output_port())
+    table_writer.set_file_name(baseline)
+    table_writer.update();
