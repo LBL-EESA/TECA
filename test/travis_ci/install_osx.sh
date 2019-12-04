2d35b74ee9927017e131a2fcb0818387dd89a453
Burlen Loring <bloring@lbl.gov>
2019-12-04 09:45:24 -0800

    update installs for mac on travis
    * cleans up all errors and warnings pertaining to updates in brew and
      the Travis CI mac environment

--- test/travis_ci/install_osx.sh
+++ test/travis_ci/install_osx.sh
@@ -3,17 +3,14 @@
 # override system install
 export PATH=/usr/local/bin:$PATH
 
-# install deps
+# install deps. note than many are included as a part of brew-core
+# these days. hence this list isn't comprehensive
 brew update
-brew tap Homebrew/homebrew-science
-brew install gcc openmpi hdf5 netcdf python python3 swig svn udunits
-# brew install boost --c++11
-pip install numpy mpi4py matplotlib
-pip3 install numpy mpi4py matplotlib
-
-# install cmake manually because brew version too old
-wget https://cmake.org/files/v3.5/cmake-3.5.2-Darwin-x86_64.tar.gz
-tar -C /usr/local -x -z -f cmake-3.5.2-Darwin-x86_64.tar.gz --strip-components=3
+brew upgrade python
+brew install openmpi swig svn udunits
+# matplotlib currently doesn't have a formula
+# teca fails to locate mpi4py installed from brew
+pip3 install mpi4py matplotlib
 
 # install data files.
 # On Apple svn is very very slow. On my mac book pro
