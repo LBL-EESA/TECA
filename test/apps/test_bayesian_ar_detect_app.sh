9f76c14ea65b72ca8e813172998bc15b50ee3c49
Burlen Loring <bloring@lbl.gov>
2020-07-08 10:21:31 -0700

    update the Bayesian AR detector test data
    Tranistions away from precipitable water field used by MW's old AR
    detector to IVT field extracted from the ARTMIP project data. The new
    test data is croped over the Pacific Coast of North America and contains
    2 AR's that evolve over 8 time steps.

--- test/apps/test_bayesian_ar_detect_app.sh
+++ test/apps/test_bayesian_ar_detect_app.sh
@@ -21,15 +21,14 @@ fi
 set -x
 
 # run the app
-${launcher} ${app_prefix}/teca_bayesian_ar_detect                       \
-    --verbose                                                           \
-    --input_regex "${data_root}/ARTMIP_MERRA_2D_201702.*\.nc$"          \
-    --cf_reader::filename_time_template "ARTMIP_MERRA_2D_%Y%m%d_%H.nc"  \
-    --cf_reader::t_axis_variable ""                                     \
-    --cf_reader::t_units "days since 1979-01-01 00:00:00"               \
-    --output_file "${data_root}/test_bayesian_ar_detect_app_output.nc"
+${launcher} ${app_prefix}/teca_bayesian_ar_detect                           \
+    --verbose --input_regex "${data_root}/ARTMIP_MERRA_2D_2017-05.*\.nc$"   \
+    --output_file test_bayesian_ar_detect_app_output.nc
 
 # run the diff
-${app_prefix}/teca_cartesian_mesh_diff                      \
-    0 "${data_root}/test_bayesian_ar_detect_app_output.nc"  \
+${app_prefix}/teca_cartesian_mesh_diff                  \
+    0 test_bayesian_ar_detect_app_output.nc             \
     "${data_root}/test_bayesian_ar_detect_app_ref.bin"
+
+# clean up
+rm test_bayesian_ar_detect_app_output.nc
