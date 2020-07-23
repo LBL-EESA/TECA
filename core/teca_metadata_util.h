aa9d98b83787fa8794d5496f3c133b25673a59b2
Burlen Loring <bloring@lbl.gov>
2020-07-22 17:06:22 -0700

    improve metadata_util documentation

--- core/teca_metadata_util.h
+++ core/teca_metadata_util.h
@@ -6,10 +6,10 @@
 
 namespace teca_metadata_util
 {
-// remove post-fix from the arrays in get_upstream_request if
-// the post-fix is set. For example if post-fix is set to "_filtered"
-// then we remove all the variables in the "arrays" set that end with 
-// this post-fix, and replace it with the actual requested array.
-void remove_post_fix(std::set<std::string> &arrays, std::string post_fix);
+// given a set of names, where names end with a common string, here called
+// a post-fix, modifies the set of names by removing the post fix from each
+// name.
+void remove_post_fix(std::set<std::string> &names, std::string post_fix);
+
 };
 #endif
