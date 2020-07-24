a62cfc768265030ba2040adccaa633db27e0beab
Burlen Loring <bloring@lbl.gov>
2020-07-24 07:27:47 -0700

    deeplab_ar_detect run the test in parallel
    * process 8 time steps, run in serial, thread only, and
      mpi+threads. exercises automatic thread load balancing.

--- test/python/test_deeplabv3p_ar_detect.py
+++ test/python/test_deeplabv3p_ar_detect.py
@@ -15,53 +15,64 @@ set_stack_trace_on_error()
 set_stack_trace_on_mpi_error()
 
 if (len(sys.argv) != 6):
-    sys.stderr.write("\n\nUsage error:\n"
-                     "test_deeplabv3p_ar_detect [deeplab model] "
-                     "[mesh data regex] [baseline mesh] "
-                     "[water vapor var] [num threads]\n\n")
+    sys.stderr.write('\n\nUsage error:\n'
+                     'test_deeplabv3p_ar_detect [deeplab model] '
+                     '[mesh data regex] [baseline] '
+                     '[water vapor var] [num threads]\n\n')
     sys.exit(-1)
 
 # parse command line
 deeplab_model = sys.argv[1]
-mesh_data_regex = sys.argv[2]
-baseline_mesh = sys.argv[3]
+input_regex = sys.argv[2]
+baseline = sys.argv[3]
 water_vapor_var = sys.argv[4]
 n_threads = int(sys.argv[5])
+vrb = 1
 
 cf_reader = teca_cf_reader.New()
-cf_reader.set_files_regex(mesh_data_regex)
+cf_reader.set_files_regex(input_regex)
 cf_reader.set_periodic_in_x(1)
 
-deeplabv3p_ar_detect = teca_deeplabv3p_ar_detect.New()
+ar_detect = teca_deeplabv3p_ar_detect.New()
+ar_detect.set_input_connection(cf_reader.get_output_port())
+ar_detect.set_verbose(vrb)
+ar_detect.set_variable_name(water_vapor_var)
+ar_detect.set_threads_per_core(1)
+ar_detect.set_thread_pool_size(n_threads)
+ar_detect.build_model(deeplab_model)
 
-deeplabv3p_ar_detect.set_input_connection(
-    cf_reader.get_output_port())
 
-deeplabv3p_ar_detect.set_variable_name(water_vapor_var)
-deeplabv3p_ar_detect.set_thread_pool_size(n_threads)
-deeplabv3p_ar_detect.build_model(deeplab_model)
+rex = teca_index_executive.New()
+rex.set_arrays(['ar_probability'])
+rex.set_verbose(vrb)
 
-if os.path.exists(baseline_mesh):
+do_test = 1
+if do_test:
     # run the test
     if rank == 0:
         sys.stdout.write('running test...\n')
 
-    baseline_mesh_reader = teca_cartesian_mesh_reader.New()
-    baseline_mesh_reader.set_file_name(baseline_mesh)
+    baseline_reader = teca_cf_reader.New()
+    baseline_reader.set_files_regex('%s-.*\\.nc'%(baseline))
+    baseline_reader.set_periodic_in_x(1)
 
     diff = teca_dataset_diff.New()
-    diff.set_input_connection(0, baseline_mesh_reader.get_output_port())
-    diff.set_input_connection(1, deeplabv3p_ar_detect.get_output_port())
+    diff.set_input_connection(0, baseline_reader.get_output_port())
+    diff.set_input_connection(1, ar_detect.get_output_port())
     diff.set_tolerance(1e-3)
+    diff.set_executive(rex)
     diff.update()
+
 else:
     # make a baseline
     if rank == 0:
-        sys.stdout.write('generating baseline %s...\n'%(baseline_mesh))
+        sys.stdout.write('generating baseline %s...\n'%(baseline))
 
-    wri = teca_cartesian_mesh_writer.New()
-    wri.set_input_connection(deeplabv3p_ar_detect.get_output_port())
-    wri.set_file_name(baseline_mesh)
+    baseline_writer = teca_cf_writer.New()
+    baseline_writer.set_input_connection(ar_detect.get_output_port())
+    baseline_writer.set_file_name('%s-%%t%%.nc'%(baseline))
+    baseline_writer.set_point_arrays(['ar_probability'])
+    baseline_writer.set_thread_pool_size(1)
 
-    wri.update()
+    baseline_writer.update()
     sys.exit(-1)
