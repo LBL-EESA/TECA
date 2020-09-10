c3fef277c2dd8092444d0138cc79f08e88394e83
Burlen Loring <bloring@lbl.gov>
2020-09-10 14:54:11 -0700

    improve temporal reduction app tests
    * update test for revisions to the app
    * add runs for daily interval, and missing value handling
    * diambiguate baseline file names

--- test/apps/test_temporal_reduction_app.sh
+++ test/apps/test_temporal_reduction_app.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 
-if [[ $# < 5 ]]
+if [[ $# < 7 ]]
 then
-    echo "usage: test_temporal_reduction_app.sh [app prefix] "   \
-         "[data root] [interval] [operator] [steps per file] " \
-         "[mpi exec] [test cores]"
+    echo "usage: test_temporal_reduction_app.sh [app prefix] " \
+         "[data root] [input regex] [array name] [interval] " \
+         "[operator] [steps per file] [mpi exec] [test cores]"
     exit -1
 fi
 
 app_prefix=${1}
 data_root=${2}
-interval=${3}
-operator=${4}
-steps_per_file=${5}
+input_regex=${3}
+array_name=${4}
+interval=${5}
+operator=${6}
+steps_per_file=${7}
 
-if [[ $# -eq 7 ]]
+if [[ $# -eq 9 ]]
 then
-    mpi_exec=${6}
-    test_cores=${7}
+    mpi_exec=${8}
+    test_cores=${9}
     launcher="${mpi_exec} -n ${test_cores}"
 fi
-
-input_regex="prw_hus_day_MRI-CGCM3_historical_r1i1p1_19500101-19501231\.nc"
-output_prefix=prw
 
 set -x
 
 # run the app
-${launcher} ${app_prefix}/teca_temporal_reduction                \
-    --input_regex "${data_root}/${input_regex}"  \
-    --interval ${interval} --operator ${operator} --arrays prw \
+${launcher} ${app_prefix}/teca_temporal_reduction \
+    --input_regex "${data_root}/${input_regex}" --interval ${interval} \
+    --operator ${operator} --point_arrays ${array_name} \
     --steps_per_file ${steps_per_file} --n_threads 2 --verbose 1 \
-    --output_file "${output_prefix}_${interval}_${operator}_%t%.nc"
+    --output_file "${array_name}_${interval}_${operator}_%t%.nc"
 
 # run the diff
-${app_prefix}/teca_cartesian_mesh_diff                           \
-    "${output_prefix}_${interval}_${operator}_.*\.nc"                   \
-    "${data_root}/${output_prefix}_${interval}_${operator}_.*\.nc"
+${app_prefix}/teca_cartesian_mesh_diff \
+    "${array_name}_${interval}_${operator}_.*\.nc" \
+    "${data_root}/test_temporal_reduction_app_${array_name}_${interval}_${operator}_.*\.nc"
 
 # clean up
-rm ${output_prefix}_${interval}_${operator}_*
+rm -f ${array_name}_${interval}_${operator}_*.nc
