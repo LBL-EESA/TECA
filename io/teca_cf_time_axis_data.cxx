7ce63939138bb4d1b7933c736cf1ca96895b70ed
Burlen Loring <bloring@lbl.gov>
2020-11-13 12:50:45 -0800

    add cf_time_axis_data
    a dataset for reading CF2 time axis in parallel using an MPI
    map reduce pattern

---
+++ io/teca_cf_time_axis_data.cxx
@@ -0,0 +1,296 @@
+#include "teca_cf_time_axis_data.h"
+
+#include "teca_binary_stream.h"
+#include "teca_dataset_util.h"
+#include "teca_string_util.h"
+#include "teca_bad_cast.h"
+
+#include <iostream>
+#include <iomanip>
+
+// some helpers to disambiguate nested pairs
+namespace {
+template <typename it_t>
+unsigned long get_file_id(const it_t &it)
+{
+    return it->first;
+}
+
+template <typename it_t>
+teca_metadata &get_metadata(const it_t &it)
+{
+    return it->second.second;
+}
+
+template <typename it_t>
+const teca_metadata &get_const_metadata(const it_t &it)
+{
+    return it->second.second;
+}
+
+
+template <typename it_t>
+p_teca_variant_array get_variant_array(const it_t &it)
+{
+    return it->second.first;
+}
+
+template <typename it_t>
+const_p_teca_variant_array get_const_variant_array(const it_t &it)
+{
+    return it->second.first;
+}
+};
+
+
+
+// --------------------------------------------------------------------------
+teca_cf_time_axis_data::teca_cf_time_axis_data()
+{
+    this->internals = new internals_t;
+}
+
+// --------------------------------------------------------------------------
+teca_cf_time_axis_data::~teca_cf_time_axis_data()
+{
+    delete this->internals;
+}
+
+// --------------------------------------------------------------------------
+void teca_cf_time_axis_data::transfer(unsigned long file_id, elem_t &&data)
+{
+    this->internals->emplace(file_id, std::forward<elem_t>(data));
+}
+
+// --------------------------------------------------------------------------
+teca_cf_time_axis_data::elem_t &teca_cf_time_axis_data::get(unsigned long file_id)
+{
+    internals_t::iterator it = this->internals->find(file_id);
+    if (it == this->internals->end())
+    {
+        TECA_ERROR("invalid file_id " << file_id)
+    }
+    return it->second;
+}
+
+// --------------------------------------------------------------------------
+const teca_cf_time_axis_data::elem_t &teca_cf_time_axis_data::get(unsigned long file_id) const
+{
+    return const_cast<teca_cf_time_axis_data*>(this)->get(file_id);
+}
+
+// --------------------------------------------------------------------------
+void teca_cf_time_axis_data::append(const const_p_teca_dataset &o)
+{
+    const_p_teca_cf_time_axis_data other =
+        std::dynamic_pointer_cast<const teca_cf_time_axis_data>(o);
+
+    if (!other)
+        throw teca_bad_cast(safe_class_name(o), "teca_cf_time_axis_data");
+
+    if (this == other.get())
+        return;
+
+    internals_t::const_iterator it = other->internals->begin();
+    internals_t::const_iterator end = other->internals->end();
+    for (; it != end; ++it)
+    {
+        p_teca_variant_array va = ::get_variant_array(it) ?
+            ::get_variant_array(it)->new_copy() : nullptr;
+
+        if (!this->internals->emplace(std::make_pair(::get_file_id(it),
+            std::make_pair(va, ::get_const_metadata(it)))).second)
+        {
+            TECA_ERROR("file_id " << ::get_file_id(it) << " is not unique");
+            return;
+        }
+    }
+}
+
+// --------------------------------------------------------------------------
+void teca_cf_time_axis_data::shallow_append(const const_p_teca_dataset &o)
+{
+    const_p_teca_cf_time_axis_data other =
+        std::dynamic_pointer_cast<const teca_cf_time_axis_data>(o);
+
+    if (!other)
+        throw teca_bad_cast(safe_class_name(o), "teca_cf_time_axis_data");
+
+    if (this == other.get())
+        return;
+
+    internals_t::const_iterator it = other->internals->begin();
+    internals_t::const_iterator end = other->internals->end();
+    for (; it != end; ++it)
+    {
+        if (!this->internals->insert(*it).second)
+        {
+            TECA_ERROR("file_id " << ::get_file_id(it) << " is not unique");
+            return;
+        }
+    }
+}
+
+// --------------------------------------------------------------------------
+int teca_cf_time_axis_data::get_type_code() const
+{
+    // FIXME -- need a user defined index
+    return (int)1e3;
+}
+
+// --------------------------------------------------------------------------
+bool teca_cf_time_axis_data::empty() const noexcept
+{
+    return this->internals->empty();
+}
+
+// --------------------------------------------------------------------------
+int teca_cf_time_axis_data::to_stream(teca_binary_stream &bs) const
+{
+    if (this->teca_dataset::to_stream(bs))
+        return -1;
+
+    unsigned long n_elem = this->internals->size();
+    bs.pack(n_elem);
+
+    internals_t::const_iterator it = this->internals->begin();
+    internals_t::const_iterator end = this->internals->end();
+    for (; it != end; ++it)
+    {
+        bs.pack(::get_file_id(it));
+
+        bs.pack(::get_const_variant_array(it)->type_code());
+
+        if (::get_const_variant_array(it)->to_stream(bs))
+            return -1;
+
+        if (::get_const_metadata(it).to_stream(bs))
+            return -1;
+    }
+
+    return 0;
+}
+
+// --------------------------------------------------------------------------
+int teca_cf_time_axis_data::from_stream(teca_binary_stream &bs)
+{
+    this->internals->clear();
+
+    if (this->teca_dataset::from_stream(bs))
+        return -1;
+
+    unsigned long n_elem = 0;
+    bs.unpack(n_elem);
+
+    for (unsigned long i = 0; i < n_elem; ++i)
+    {
+        unsigned long file_id = 0;
+        bs.unpack(file_id);
+
+        unsigned int type_code;
+        bs.unpack(type_code);
+
+        p_teca_variant_array va = teca_variant_array_factory::New(type_code);
+
+        if (va->from_stream(bs))
+            return -1;
+
+        teca_metadata md;
+        if (md.from_stream(bs))
+            return -1;
+
+        this->internals->emplace(std::make_pair(file_id, std::make_pair(va, md)));
+    }
+
+    return 0;
+}
+
+// --------------------------------------------------------------------------
+int teca_cf_time_axis_data::to_stream(std::ostream &os) const
+{
+    internals_t::const_iterator it = this->internals->begin();
+    internals_t::const_iterator end = this->internals->end();
+    for (; it != end; ++it)
+    {
+        std::cerr << std::setw(6) << ::get_file_id(it) << " ";
+
+        ::get_const_variant_array(it)->to_stream(os);
+
+        os << std::endl;
+    }
+    return 0;
+}
+
+// --------------------------------------------------------------------------
+int teca_cf_time_axis_data::from_stream(std::istream &)
+{
+    TECA_ERROR("teca_cf_time_axis_data::from_stream not implemented")
+    return -1;
+}
+
+// --------------------------------------------------------------------------
+void teca_cf_time_axis_data::copy(const const_p_teca_dataset &o)
+{
+    const_p_teca_cf_time_axis_data other =
+        std::dynamic_pointer_cast<const teca_cf_time_axis_data>(o);
+
+    if (!other)
+        throw teca_bad_cast(safe_class_name(o), "teca_cf_time_axis_data");
+
+    if (this == other.get())
+        return;
+
+    this->internals->clear();
+
+    internals_t::const_iterator it = other->internals->begin();
+    internals_t::const_iterator end = other->internals->end();
+    for (; it != end; ++it)
+    {
+        p_teca_variant_array va = ::get_variant_array(it) ?
+             ::get_variant_array(it)->new_copy() : nullptr;
+
+        this->internals->emplace(std::make_pair(::get_file_id(it),
+            std::make_pair(va, ::get_const_metadata(it))));
+    }
+}
+
+// --------------------------------------------------------------------------
+void teca_cf_time_axis_data::shallow_copy(const p_teca_dataset &o)
+{
+    const_p_teca_cf_time_axis_data other =
+        std::dynamic_pointer_cast<const teca_cf_time_axis_data>(o);
+
+    if (!other)
+        throw teca_bad_cast(safe_class_name(o), "teca_cf_time_axis_data");
+
+    if (this == other.get())
+        return;
+
+    this->internals->clear();
+
+    internals_t::const_iterator it = other->internals->begin();
+    internals_t::const_iterator end = other->internals->end();
+    for (; it != end; ++it)
+    {
+        unsigned long id = ::get_file_id(it);
+        this->internals->emplace(id, it->second);
+    }
+}
+
+// --------------------------------------------------------------------------
+void teca_cf_time_axis_data::swap(p_teca_dataset &o)
+{
+    p_teca_cf_time_axis_data other =
+        std::dynamic_pointer_cast<teca_cf_time_axis_data>(o);
+
+    if (!other)
+        throw teca_bad_cast(safe_class_name(o), "teca_cf_time_axis_data");
+
+    if (this == other.get())
+        return;
+
+    internals_t *tmp = this->internals;
+    this->internals = other->internals;
+    other->internals = tmp;
+}
+
---
+++ io/teca_cf_time_axis_data.h
@@ -0,0 +1,108 @@
+#ifndef teca_cf_time_axis_data_h
+#define teca_cf_time_axis_data_h
+
+#include "teca_dataset.h"
+#include "teca_variant_array.h"
+#include "teca_metadata.h"
+
+#include <map>
+#include <vector>
+#include <string>
+
+#include "teca_shared_object.h"
+TECA_SHARED_OBJECT_FORWARD_DECL(teca_cf_time_axis_data)
+
+/**
+A dataset used to read NetCDF CF2 time and metadata in parallel
+*/
+class teca_cf_time_axis_data : public teca_dataset
+{
+public:
+    TECA_DATASET_STATIC_NEW(teca_cf_time_axis_data)
+    TECA_DATASET_NEW_INSTANCE()
+    TECA_DATASET_NEW_COPY()
+
+    ~teca_cf_time_axis_data() override;
+
+    using elem_t = std::pair<p_teca_variant_array, teca_metadata>;
+
+    // transfer the element associated with file to the dataset.
+    // after transfer the passed element is invalid in the calling context
+    void transfer(unsigned long file_id, elem_t &&data);
+
+    // access the file's element
+    elem_t &get(unsigned long file_id);
+    const elem_t &get(unsigned long file_id) const;
+
+    // given an element extract metadata
+    static
+    teca_metadata &get_metadata(elem_t&elem)
+    { return elem.second; }
+
+    static
+    const teca_metadata &get_metadata(const elem_t &elem)
+    { return elem.second; }
+
+    // given an element extract the time axis
+    static
+    p_teca_variant_array get_variant_array(elem_t &elem)
+    { return elem.first; }
+
+    static
+    const_p_teca_variant_array get_variant_array(const elem_t &elem)
+    { return elem.first; }
+
+    // append the data from the other instance
+    void append(const const_p_teca_dataset &other);
+    void shallow_append(const const_p_teca_dataset &other);
+
+    // return a unique string identifier
+    std::string get_class_name() const override
+    { return "teca_cf_time_axis_data"; }
+
+    // return an integer identifier uniquely naming the dataset type
+    int get_type_code() const override;
+
+    // covert to bool. true if the dataset is not empty.
+    // otherwise false.
+    explicit operator bool() const noexcept
+    { return !this->empty(); }
+
+    // return true if the dataset is empty.
+    bool empty() const noexcept override;
+
+    // serialize the dataset to/from the given stream
+    // for I/O or communication
+    int to_stream(teca_binary_stream &) const override;
+    int from_stream(teca_binary_stream &) override;
+
+    // stream to/from human readable representation
+    int to_stream(std::ostream &) const override;
+    int from_stream(std::istream &) override;
+
+    // copy data and metadata. shallow copy uses reference
+    // counting, while copy duplicates the data.
+    void copy(const const_p_teca_dataset &other) override;
+
+    // deep copy a subset of row values.
+    void copy(const const_p_teca_cf_time_axis_data &other,
+        unsigned long first_row, unsigned long last_row);
+
+    void shallow_copy(const p_teca_dataset &other) override;
+
+    // swap internals of the two objects
+    void swap(p_teca_dataset &other) override;
+
+protected:
+    teca_cf_time_axis_data();
+
+    teca_cf_time_axis_data(const teca_cf_time_axis_data &other) = delete;
+    teca_cf_time_axis_data(teca_cf_time_axis_data &&other) = delete;
+    teca_cf_time_axis_data &operator=(const teca_cf_time_axis_data &other) = delete;
+
+private:
+    using internals_t = std::map<unsigned long, elem_t>;
+    internals_t *internals;
+};
+
+#endif
