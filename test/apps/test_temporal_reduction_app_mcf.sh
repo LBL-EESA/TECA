8d213b5e29a997f7088a19c1433898d937467a0b
Burlen Loring <bloring@lbl.gov>
2020-11-30 20:02:46 -0800

    temporal_reduction app integrate multi_cf_reader
    * support for HighResMIP datasets
    * adds new tests of the capability

--- test/apps/test_temporal_reduction_app_mcf.sh
+++ test/apps/test_temporal_reduction_app_mcf.sh
@@ -3,14 +3,14 @@
 if [[ $# < 7 ]]
 then
     echo "usage: test_temporal_reduction_app.sh [app prefix] " \
-         "[data root] [input regex] [array name] [interval] " \
+         "[data root] [input file] [array name] [interval] " \
          "[operator] [steps per file] [mpi exec] [test cores]"
     exit -1
 fi
 
 app_prefix=${1}
 data_root=${2}
-input_regex=${3}
+input_file=${3}
 array_name=${4}
 interval=${5}
 operator=${6}
@@ -29,10 +29,10 @@ test_name=test_temporal_reduction
 output_base=${test_name}_${array_name}_${interval}_${operator}
 
 # run the app
-time ${launcher} ${app_prefix}/teca_temporal_reduction                  \
-    --input_regex "${data_root}/${input_regex}" --interval ${interval}  \
-    --operator ${operator} --point_arrays ${array_name}                 \
-    --steps_per_file ${steps_per_file} --n_threads 2 --verbose 1        \
+time ${launcher} ${app_prefix}/teca_temporal_reduction                                  \
+    --input_file "${app_prefix}/../test/ECMWF-IFS-HR-SST-present.mcf"                   \
+    --interval ${interval} --operator ${operator} --point_arrays ${array_name}          \
+    --z_axis_variable plev --steps_per_file ${steps_per_file} --n_threads 2 --verbose 1 \
     --output_file "${output_base}_%t%.nc"
 
 do_test=1
@@ -42,11 +42,13 @@ then
     cp -vd ${output_base}_*.nc ${data_root}/
 else
     # run the diff
-    time ${app_prefix}/teca_cartesian_mesh_diff                     \
-        --reference_dataset "${data_root}/${output_base}_.*\.nc"    \
-        --test_dataset "${output_base}_.*\.nc"                      \
+    time ${app_prefix}/teca_cartesian_mesh_diff                                 \
+        --reference_dataset "${data_root}/${output_base}_.*\.nc"                \
+        --test_dataset "${output_base}_.*\.nc"                                  \
+        --ref_reader::z_axis_variable plev --test_reader::z_axis_variable plev  \
         --arrays ${array_name} --verbose
 
     # clean up
     rm ${output_base}_*.nc
 fi
+
