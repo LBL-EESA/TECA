6c41864a4061ad72ed95ae3481fe0a9c1dfab1e1
Burlen Loring <bloring@lbl.gov>
2020-12-18 21:15:26 -0800

    common add ostream operators for vector

---
+++ core/teca_common.cxx
@@ -0,0 +1,23 @@
+#include "teca_common.h"
+
+// **************************************************************************
+std::ostream &operator<<(std::ostream &os, const std::vector<std::string> &vec)
+{
+    if (!vec.empty())
+    {
+        os << "\"" << vec[0] << "\"";
+        size_t n = vec.size();
+        for (size_t i = 1; i < n; ++i)
+            os << ", \"" << vec[i] << "\"";
+    }
+    return os;
+}
+
+// **************************************************************************
+int have_tty()
+{
+    static int have = -1;
+    if (have < 0)
+        have = isatty(fileno(stderr));
+    return have;
+}
