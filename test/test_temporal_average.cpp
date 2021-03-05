d470100e82aaab8ff042dcb5bff3480098e03e43
Burlen Loring <bloring@lbl.gov>
2021-03-05 09:22:06 -0800

    add indexed_dataset_cache
    * teca_indexed_dataset_cache an algorithm that implements a thread safe cache
      where when predetermined maximum cahce size is exceeded the least recently used
      dataset is removed from the cache. This algorithm is used to reduce I/O demand
      in applications that repeatedly access the same datasets. Examples
      include computing a moving average over time dimension, and serving
      land-sea mask, or surface elevation field.
    * updated the temporal_average test to use the new algorithm.
    * did preliminary testing of threaded execution, but a threaded test
      still needs to be developed

--- test/test_temporal_average.cpp
+++ test/test_temporal_average.cpp
@@ -1,68 +1,107 @@
 #include "teca_cf_reader.h"
 #include "teca_normalize_coordinates.h"
+#include "teca_indexed_dataset_cache.h"
 #include "teca_temporal_average.h"
 #include "teca_cartesian_mesh_writer.h"
+#include "teca_cf_writer.h"
+#include "teca_dataset_diff.h"
 #include "teca_index_executive.h"
 #include "teca_system_interface.h"
+#include "teca_system_util.h"
 
 #include <vector>
 #include <string>
 #include <iostream>
 using namespace std;
 
-// example use
-// ./test/test_cf_reader ~/work/teca/data/'cam5_1_amip_run2.cam2.h2.1991-10-.*' tmp 0 -1 PS
-
 int main(int argc, char **argv)
 {
     teca_system_interface::set_stack_trace_on_error();
 
-    if (argc < 7)
+    if (argc < 8)
     {
-        cerr << endl << "Usage error:" << endl
-            << "test_cf_reader [input regex] [output] [first step] [last step] [filter width] [array] [array] ..." << endl
-            << endl;
+        std::cerr << std::endl << "Usage error:" << std::endl
+            << "test_temporal_average [input regex] [baseline] [first step] [last step]"
+               "  [filter width] [n threads] [array] [array] ..." << std::endl
+            << std::endl;
         return -1;
     }
 
     // parse command line
     string regex = argv[1];
-    string output = argv[2];
+    string baseline = argv[2];
     long first_step = atoi(argv[3]);
     long last_step = atoi(argv[4]);
     int filter_width = atoi(argv[5]);
-    vector<string> arrays;
-    arrays.push_back(argv[6]);
-    for (int i = 7; i < argc; ++i)
+    int n_threads = atoi(argv[6]);
+    std::vector<std::string> arrays;
+    arrays.push_back(argv[7]);
+    for (int i = 8; i < argc; ++i)
         arrays.push_back(argv[i]);
 
     // create the cf reader
     p_teca_cf_reader r = teca_cf_reader::New();
     r->set_files_regex(regex);
 
+    // normalize coords
     p_teca_normalize_coordinates c = teca_normalize_coordinates::New();
     c->set_input_connection(r->get_output_port());
 
+    // ds cache
+    p_teca_indexed_dataset_cache dsc = teca_indexed_dataset_cache::New();
+    dsc->set_input_connection(c->get_output_port());
+    dsc->set_max_cache_size(2*n_threads*filter_width);
+
+    // temporal avg
     p_teca_temporal_average a = teca_temporal_average::New();
     a->set_filter_width(filter_width);
     a->set_filter_type(teca_temporal_average::backward);
-    a->set_input_connection(c->get_output_port());
+    a->set_input_connection(dsc->get_output_port());
 
-    // create the vtk writer connected to the cf reader
-    p_teca_cartesian_mesh_writer w = teca_cartesian_mesh_writer::New();
-    w->set_file_name(output);
-    w->set_input_connection(a->get_output_port());
 
-    // set the executive on the writer to stream time steps
-    p_teca_index_executive exec = teca_index_executive::New();
-    exec->set_start_index(first_step);
-    exec->set_end_index(last_step);
-    exec->set_arrays(arrays);
+    bool do_test = true;
+    teca_system_util::get_environment_variable("TECA_DO_TEST", do_test);
+    if (do_test)
+    {
+        std::cerr << "running the test..." << std::endl;
 
-    w->set_executive(exec);
+        baseline += ".*\\.nc$";
 
-    // run the pipeline
-    w->update();
+        p_teca_cf_reader br = teca_cf_reader::New();
+        br->set_files_regex(baseline);
+
+        // executive
+        p_teca_index_executive rex = teca_index_executive::New();
+        rex->set_start_index(first_step);
+        rex->set_end_index(last_step);
+        rex->set_arrays(arrays);
+
+        p_teca_dataset_diff diff = teca_dataset_diff::New();
+        diff->set_input_connection(0, br->get_output_port());
+        diff->set_input_connection(1, a->get_output_port());
+        diff->set_verbose(1);
+        diff->set_executive(rex);
+        // TODO : test with threads
+        //diff->set_thread_pool_size(n_threads);
+        diff->update();
+    }
+    else
+    {
+        std::cerr << "writing the baseline..." << std::endl;
+
+        baseline += "_%t%.nc";
+
+        // writer
+        p_teca_cf_writer w = teca_cf_writer::New();
+        w->set_input_connection(a->get_output_port());
+        w->set_thread_pool_size(n_threads);
+        w->set_point_arrays(arrays);
+        w->set_file_name(baseline);
+        w->set_first_step(first_step);
+        w->set_last_step(last_step);
+        w->set_steps_per_file(10000);
+        w->update();
+    }
 
     return 0;
 }
