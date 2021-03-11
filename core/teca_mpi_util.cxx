b7bfd55bddb1456d636e50dfcd5117123b4d3774
Burlen Loring <bloring@lbl.gov>
2021-03-10 22:34:30 -0800

    mpi_util split a communicator into groups of a given size

--- core/teca_mpi_util.cxx
+++ core/teca_mpi_util.cxx
@@ -22,7 +22,7 @@ int equipartition_communicator(MPI_Comm comm,
 
         if (n_ranks < new_comm_size)
         {
-            // can't increase beyond the original sizew
+            // can't increase beyond the original size
             return 0;
         }
 
@@ -47,6 +47,41 @@ int equipartition_communicator(MPI_Comm comm,
         // clean up
         MPI_Group_free(&world_group);
         MPI_Group_free(&new_group);
+    }
+#endif
+    return 0;
+}
+
+// **************************************************************************
+int split_communicator(MPI_Comm world_comm,
+    int group_size, MPI_Comm *group_comm)
+{
+#if defined(TECA_HAS_MPI)
+    int is_init = 0;
+    MPI_Initialized(&is_init);
+    if (is_init)
+    {
+        int world_rank = 0;
+        int world_size = 1;
+
+        MPI_Comm_rank(world_comm, &world_rank);
+        MPI_Comm_size(world_comm, &world_size);
+
+        MPI_Group world_group = MPI_GROUP_EMPTY;
+        MPI_Comm_group(world_comm, &world_group);
+
+        int group_id = world_rank / group_size;
+        int group_start = group_id * group_size;
+        int group_end = std::min(world_size, group_start + group_size);
+        int group_range[3] = {group_start, group_end, 1};
+
+        MPI_Group sub_group = MPI_GROUP_EMPTY;
+        MPI_Group_range_incl(world_group, 1, &group_range, &sub_group);
+
+        MPI_Comm_create(world_comm, sub_group, group_comm);
+
+        MPI_Group_free(&world_group);
+        MPI_Group_free(&sub_group);
     }
 #endif
     return 0;
