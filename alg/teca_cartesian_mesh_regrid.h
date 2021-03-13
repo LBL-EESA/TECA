1f486104280f9d0b0422c2237e97b3d6d393a334
Burlen Loring <bloring@lbl.gov>
2021-03-12 16:07:15 -0800

    cartesian_mesh_regrid update class doc

--- alg/teca_cartesian_mesh_regrid.h
+++ alg/teca_cartesian_mesh_regrid.h
@@ -11,19 +11,22 @@
 
 TECA_SHARED_OBJECT_FORWARD_DECL(teca_cartesian_mesh_regrid)
 
-/// transfer data between overlapping meshes of potentially different resolution
-/**
-an algorithm that transfers data between cartesian meshes defined in the same
-world coordinate system but potentially different resolutions. nearest or
-linear interpolation are supported.
-
-By default the first input is the target mesh. the second input is the source
-mesh. This can be changed by setting the target_input property.
-
-the arrays to move from source to target can be selected using add_array api or
-in the request key "arrays". this is a spatial regriding operation
-for temporal regriding see teca_mesh_temporal_regrid.
-*/
+/** @brief
+ * Transfers data between spatially overlapping meshes of potentially different
+ * resolutions.
+ *
+ * @details
+ * an algorithm that transfers data between cartesian meshes defined in the
+ * same world coordinate system but potentially different resolutions. nearest
+ * or linear interpolation are supported.
+ *
+ * By default the first input is the target mesh. the second input is the
+ * source mesh. This can be changed by setting the target_input property.
+ *
+ * the arrays to move from source to target can be selected using add_array api
+ * or in the request key "arrays". this is a spatial regriding operation for
+ * temporal regriding see teca_mesh_temporal_regrid.
+ */
 class teca_cartesian_mesh_regrid : public teca_algorithm
 {
 public:
@@ -37,23 +40,36 @@ public:
     TECA_GET_ALGORITHM_PROPERTIES_DESCRIPTION()
     TECA_SET_ALGORITHM_PROPERTIES()
 
-    // set the list of arrays to move from the source
-    // to the target
+    /** @anchor array
+     * @name array
+     * set the list of arrays to move from the source to the target
+     */
+    //@{
     TECA_ALGORITHM_VECTOR_PROPERTY(std::string, array)
+    //@}
 
-    // set the input connection from which metadata such as arrays
-    // and time steps are taken from.
+    /** @anchor target_input
+     * @name target_input
+     * set the input connection which provides the output geometry.
+     */
+    ///@{
     TECA_ALGORITHM_PROPERTY(int, target_input)
+    ///@}
 
-    // set the interpolation mode used in transfering
-    // data between meshes of differing resolution.
-    // in nearest mode value at the nearest grid point
-    // is used, in linear mode bi/tri linear interpolation
-    // is used.
+    /** @anchor interpolation_mode
+     * @name interpolation_mode
+     * set the interpolation mode used in transfering data between meshes of
+     * differing resolution.  in nearest mode value at the nearest grid point
+     * is used, in linear mode bi/tri linear interpolation is used.
+     */
+    //@{
     enum {nearest=0, linear=1};
     TECA_ALGORITHM_PROPERTY(int, interpolation_mode)
     void set_interpolation_mode_nearest(){ interpolation_mode = nearest; }
     void set_interpolation_mode_linear(){ interpolation_mode = linear; }
+    //@}
+
+
 
 protected:
     teca_cartesian_mesh_regrid();
