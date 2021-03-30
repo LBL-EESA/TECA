9c5ca3a29315046b39a8b44ce84eab9281b893aa
Burlen Loring <bloring@lbl.gov>
2021-03-30 12:58:29 -0700

    metadata_util add get_array_extent
    adds a function to get an array's valid extent from the mesh
    extent and array attributes.

--- core/teca_metadata_util.cxx
+++ core/teca_metadata_util.cxx
@@ -1,9 +1,37 @@
 #include "teca_metadata_util.h"
 
+#include "teca_metadata.h"
+#include "teca_common.h"
+
 namespace teca_metadata_util
 {
 
+// **************************************************************************
+int get_array_extent(const teca_metadata &array_attributes,
+    const unsigned long mesh_extent[6], unsigned long array_extent[6])
+{
+    for (int i = 0; i < 6; ++i)
+        array_extent[i] = mesh_extent[i];
+
+    unsigned long dim_active[4] = {0ul};
+    if (array_attributes.get("mesh_dim_active", dim_active, 4))
     {
+        //TECA_ERROR("metadata issue. The array attributes collection is"
+        //    " missing the mesh_dim_active key")
+        return -1;
     }
+
+    // make the extent 1 in any direction that this array is undefined in
+    if (!dim_active[0])
+        array_extent[1] = array_extent[0] = 0;
+
+    if (!dim_active[1])
+        array_extent[3] = array_extent[2] = 0;
+
+    if (!dim_active[2])
+        array_extent[5] = array_extent[4] = 0;
+
+    return 0;
 }
+
 };
--- core/teca_metadata_util.h
+++ core/teca_metadata_util.h
@@ -1,8 +1,18 @@
 #ifndef teca_metadata_util_h
 #define teca_metadata_util_h
 
+class teca_metadata;
 
 namespace teca_metadata_util
 {
+/** Given a collection of array attributes (following the conventions used by
+ * the teca_cf_reader) and a mesh extent, compute and return the valid extent
+ * of the array. This takes into account 1d and 2d arrays on a 3d mesh. Return
+ * zero if successful. The mesh_dims_active key is required, if not found 1
+ * is returned and the array_extent is set to the mesh_extent.
+ */
+int get_array_extent(const teca_metadata &array_attributes,
+    const unsigned long mesh_extent[6], unsigned long array_extent[6]);
 };
+
 #endif
