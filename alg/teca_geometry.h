1b2fc6dfb18dccb7649f757d24f0837e4f4982b4
Burlen Loring <bloring@lbl.gov>
2021-06-08 15:05:11 -0700

    Adds teca_geometry::polygon
    a container for storing the vertices of a polygon.

--- alg/teca_geometry.h
+++ alg/teca_geometry.h
@@ -1,11 +1,17 @@
 #ifndef teca_geometry_h
 #define teca_geometry_h
 
+#include "teca_binary_stream.h"
+
+#include <memory>
+#include <limits>
+
 /// @file
 
 /// Codes dealing with computational geometry
 namespace teca_geometry
 {
+
 /// tests if a point is Left|On|Right of an infinite line.
 template<typename n_t>
 bool left(n_t e0x, n_t e0y, n_t e1x, n_t e1y, n_t px, n_t py)
@@ -56,6 +62,124 @@ bool point_in_poly(n_t px, n_t py,
         }
     }
     return wn;
+}
+
+/** @breif  The vertices of a 2D polygon in clockwise order. The first
+ * and last point are required to be the same.
+ */
+template <typename coord_t>
+struct polygon
+{
+    polygon() : vx(nullptr), vy(nullptr), n_verts(0) {}
+
+    /// deep copy the vertices of the polygon.
+    template <typename in_coord_t>
+    void copy(const in_coord_t *in_vx,
+        const in_coord_t *in_vy, unsigned long in_n_verts);
+
+    /// transforms coordinates to be in [0 360 -90 90]
+    void normalize_coordinates();
+
+    /// returns true if a point is inside the polygon
+    bool inside(coord_t px, coord_t py);
+
+    /** compute an axis aligned bounding box around the polygon
+     * with the layout [x0 x1 y0 y1].
+     */
+    void get_bounds(coord_t *bounds);
+
+    /// serialize to the given stream
+    void to_stream(teca_binary_stream &bs);
+
+    /// deseriealize from the given stream
+    int from_stream(teca_binary_stream &bs);
+
+    std::shared_ptr<coord_t> vx;
+    std::shared_ptr<coord_t> vy;
+    unsigned long n_verts;
+};
+
+
+// --------------------------------------------------------------------------
+template <typename coord_t>
+template <typename in_coord_t>
+void polygon<coord_t>::copy(const in_coord_t *in_vx,
+    const in_coord_t *in_vy, unsigned long in_n_verts)
+{
+    unsigned long nbytes = in_n_verts*sizeof(coord_t);
+
+    vx = std::shared_ptr<coord_t>((coord_t*)malloc(nbytes), free);
+    memcpy(vx.get(), in_vx, nbytes);
+
+    vy = std::shared_ptr<coord_t>((coord_t*)malloc(nbytes), free);
+    memcpy(vy.get(), in_vy, nbytes);
+
+    n_verts = in_n_verts;
+}
+
+// --------------------------------------------------------------------------
+template <typename coord_t>
+void polygon<coord_t>::normalize_coordinates()
+{
+    coord_t *pvx = vx.get();
+    for (unsigned long i = 0; i < n_verts; ++i)
+        pvx[i] = pvx[i] < coord_t(0) ? pvx[i] + coord_t(360) : pvx[i];
+}
+
+// --------------------------------------------------------------------------
+template <typename coord_t>
+bool polygon<coord_t>::inside(coord_t px, coord_t py)
+{
+    return teca_geometry::point_in_poly(px, py, vx.get(), vy.get(), n_verts);
+}
+
+// --------------------------------------------------------------------------
+template <typename coord_t>
+void polygon<coord_t>::get_bounds(coord_t *bounds)
+{
+    const coord_t *pvx = vx.get();
+    const coord_t *pvy = vy.get();
+
+    bounds[0] = std::numeric_limits<coord_t>::max();
+    for (unsigned long i = 0; i < n_verts; ++i)
+        bounds[0] = pvx[i] < bounds[0] ? pvx[i] : bounds[0];
+
+    bounds[1] = std::numeric_limits<coord_t>::lowest();
+    for (unsigned long i = 0; i < n_verts; ++i)
+        bounds[1] = pvx[i] > bounds[1] ? pvx[i] : bounds[1];
+
+    bounds[2] = std::numeric_limits<coord_t>::max();
+    for (unsigned long i = 0; i < n_verts; ++i)
+        bounds[2] = pvy[i] < bounds[2] ? pvy[i] : bounds[2];
+
+    bounds[3] = std::numeric_limits<coord_t>::lowest();
+    for (unsigned long i = 0; i < n_verts; ++i)
+        bounds[3] = pvy[i] > bounds[3] ? pvy[i] : bounds[3];
+}
+
+// --------------------------------------------------------------------------
+template <typename coord_t>
+void polygon<coord_t>::to_stream(teca_binary_stream &bs)
+{
+    bs.pack(n_verts);
+    bs.pack(vx.get(), n_verts);
+    bs.pack(vy.get(), n_verts);
+}
+
+// --------------------------------------------------------------------------
+template <typename coord_t>
+int polygon<coord_t>::from_stream(teca_binary_stream &bs)
+{
+    bs.unpack(n_verts);
+
+    unsigned long nbytes = n_verts*sizeof(coord_t);
+    vx = std::shared_ptr<coord_t>((coord_t*)malloc(nbytes), free);
+    vy = std::shared_ptr<coord_t>((coord_t*)malloc(nbytes), free);
+
+    bs.unpack(vx.get(), n_verts);
+    bs.unpack(vy.get(), n_verts);
+
+    return 0;
 }
 
 };
