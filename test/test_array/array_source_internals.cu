6a0416125ab3f23fa2f6fae07370296cb0c06506
Burlen Loring <bloring@lbl.gov>
2021-09-04 12:01:21 -0700

    test_array array_source CUDA implementation

---
+++ test/test_array/array_source_internals.cu
@@ -0,0 +1,62 @@
+#include "array_source_internals.h"
+
+#include "teca_cuda_mm_allocator.h"
+#include "teca_cuda_util.h"
+
+namespace array_source_internals
+{
+namespace gpu
+{
+// **************************************************************************
+template<typename data_t>
+__global__
+void initialize(data_t *data, double val, size_t n_vals)
+{
+    unsigned long i = teca_cuda_util::thread_id_to_array_index();
+
+    if (i >= n_vals)
+        return;
+
+    data[i] = val;
+}
+}
+
+// **************************************************************************
+int cuda_dispatch(int device_id, p_array &a_out, double val, size_t n_vals)
+{
+#ifndef TECA_NDEBUG
+    std::cerr << teca_parallel_id()
+        << "array_source_internals::cuda_dispatch device_id="
+        << device_id << std::endl;
+#endif
+    // set the device
+    cudaError_t ierr = cudaSuccess;
+    if ((ierr = cudaSetDevice(device_id)) != cudaSuccess)
+    {
+        TECA_ERROR("Failed to set the CUDA device to " << device_id)
+        return -1;
+    }
+
+    // allocate the memory
+    a_out = array::new_cuda_accessible();
+    a_out->resize(n_vals);
+
+    // determine kernel launch parameters
+    dim3 block_grid;
+    int n_blocks = 0;
+    dim3 thread_grid;
+    if (teca_cuda_util::partition_thread_blocks(0, n_vals,
+        8, block_grid, n_blocks, thread_grid))
+    {
+        TECA_ERROR("Failed to partition thread blocks")
+        return -1;
+    }
+
+    // initialize the data
+    array_source_internals::gpu::initialize
+        <<<block_grid, thread_grid>>>(a_out->get(), val, n_vals);
+
+    return 0;
+}
+
+}
---
+++ test/test_array/array_source_internals.cxx
@@ -0,0 +1,34 @@
+#include "array_source_internals.h"
+
+namespace array_source_internals
+{
+// **************************************************************************
+int cpu_dispatch(p_array &a_out, double val, size_t n_vals)
+{
+#ifndef TECA_NDEBUG
+    std::cerr << teca_parallel_id()
+        << "array_source_internals::cpu_dispatch" << std::endl;
+#endif
+    a_out = array::new_cpu_accessible();
+    a_out->resize(n_vals);
+
+    array_source_internals::cpu::initialize(a_out->get(), n_vals, val);
+
+    return 0;
+}
+
+#if !defined(TECA_HAS_CUDA)
+// **************************************************************************
+int cuda_dispatch(int device_id, p_array &a_out, double val, size_t n_vals)
+{
+    (void) device_id;
+    (void) a_out;
+    (void) val;
+    (void) n_vals;
+
+    TECA_ERROR("array_source failed because CUDA is not available")
+
+    return -1;
+}
+#endif
+}
