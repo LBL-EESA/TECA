5efd08ca8f36998c52381198b773a627778ede77
Burlen Loring <bloring@lbl.gov>
2022-01-19 15:57:44 -0800

    doc improve the index_reduce documentation

--- core/teca_index_reduce.h
+++ core/teca_index_reduce.h
@@ -11,30 +11,25 @@
 
 TECA_SHARED_OBJECT_FORWARD_DECL(teca_index_reduce)
 
-/// Base class for MPI + threads map reduce reduction over an index.
+/// Base class for MPI+threads+GPUs map reduce reduction over a set of indices.
 /**
- * The available indices are partitioned across MPI ranks and threads. One can
- * restrict operation to a range of time steps by setting first and last
- * indices to process.
+ * The available indices are partitioned across MPI ranks and threads. Threads
+ * are assigned to service GPUs or CPU cores. One can restrict operation to a
+ * range of time steps by setting first and last indices to process.
  *
- * metadata keys:
+ * ### metadata keys:
  *
- *      requires:
- *
- *      index_initializer_key -- holds the name of the key that tells how
- *                               many indices are available. the named key
- *                               must also be present and should contain the
- *                               number of indices available
- *
- *      index_request_key -- holds the name of the key used to request
- *                           a specific index. request are generated with this
- *                           name set to a specific index to be processed some
- *                           upstream algorithm is expected to produce the
- *                           data associated with the given index
- *
- *      consumes:
- *
- *      The key named by index_request_key
+ *  | key                   | description |
+ *  | ----                  | ----------- |
+ *  | index_initializer_key | Holds the name of the key that tells how many |
+ *  |                       | indices are available. The named key must also |
+ *  |                       | be present and should contain the number of |
+ *  |                       | indices available. |
+ *  | index_request_key     | Holds the name of the key used to request a |
+ *  |                       | specific index. Requests are generated with this |
+ *  |                       | name set to a specific index to be processed |
+ *  |                       | some upstream algorithm is expected to produce |
+ *  |                       | the data associated with the given index. |
  */
 class TECA_EXPORT teca_index_reduce : public teca_threaded_algorithm
 {
@@ -47,12 +42,22 @@ public:
     TECA_GET_ALGORITHM_PROPERTIES_DESCRIPTION()
     TECA_SET_ALGORITHM_PROPERTIES()
 
-    // set the range of time steps to process.
-    // setting first_step=0 and last_step=-1 results
-    // in processing all available steps. this is
-    // the default.
+    /** @name start_index
+     * set the first index to process. Indices go from 0 to n-1. The default
+     * start_index is 0.
+     */
+    ///@{
     TECA_ALGORITHM_PROPERTY(long, start_index)
+    ///@}
+
+    /** @name end_index
+     * set the last index to process. Indices go from 0 to n-1. The default
+     * end_index is -1, which is used to indicate all indices should be
+     * processed.
+     */
+    ///@{
     TECA_ALGORITHM_PROPERTY(long, end_index)
+    ///@}
 
 protected:
     teca_index_reduce();
@@ -92,18 +97,19 @@ protected:
         return std::const_pointer_cast<teca_dataset>(ds);
     }
 
-    // override that allows derived classes to generate upstream
-    // requests that will be applied over all time steps. derived
-    // classes implement this method instead of get_upstream_request,
-    // which here is already implemented to handle the application
-    // of requests over all timesteps.
+    /** An override that allows derived classes to generate upstream requests
+     * that will be applied over all time steps. derived classes implement this
+     * method instead of ::get_upstream_request, which here is already
+     * implemented to handle the application of requests over the index set.
+     */
     virtual std::vector<teca_metadata> initialize_upstream_request(
         unsigned int port, const std::vector<teca_metadata> &input_md,
         const teca_metadata &request) = 0;
 
-    // override that allows derived classes to report what they can
-    // produce. this will be called from get_output_metadata which
-    // will strip out time and partition time across MPI ranks.
+    /** An override that allows derived classes to report what they can
+     * produce. this will be called from ::get_output_metadata which will strip
+     * out time and partition time across MPI ranks.
+     */
     virtual teca_metadata initialize_output_metadata(unsigned int port,
         const std::vector<teca_metadata> &input_md) = 0;
 
@@ -114,34 +120,38 @@ protected:
     using teca_threaded_algorithm::get_output_metadata;
     using teca_threaded_algorithm::execute;
 
-    // generates an upstream request for each timestep. will
-    // call initialize_upstream_request and apply the results to
-    // all time steps.
+    /** Generates an upstream request for each index. will call
+     * initialize_upstream_request and apply the results to all time steps.
+     */
     std::vector<teca_metadata> get_upstream_request(
         unsigned int port, const std::vector<teca_metadata> &input_md,
         const teca_metadata &request) override;
 
-    // uses MPI communication to collect remote data for
-    // required for the reduction. calls "reduce" with
-    // each pair of datasets until the datasets across
-    // all threads and ranks are reduced into a single
-    // dataset, which is returned.
+    /** Uses MPI communication to collect remote data for required for the
+     * reduction. Calls ::reduce with each pair of datasets until the datasets
+     * across all threads and ranks are reduced into a single dataset, which is
+     * returned.
+     */
     const_p_teca_dataset execute(unsigned int port,
         const std::vector<const_p_teca_dataset> &input_data,
         const teca_metadata &request, int streaming) override;
 
-    // consumes time metadata, partitions time's across
-    // MPI ranks.
+    /// consumes index metadata, and partitions indices across MPI ranks.
     teca_metadata get_output_metadata(unsigned int port,
         const std::vector<teca_metadata> &input_md) override;
 
 private:
-    // drivers for reducing the local and remote datasets.
-    // calls reduce override as needed.
-    const_p_teca_dataset reduce_local(
+    /** The driver for reducing the local datasets. Calls the ::reduce override as
+     * needed.
+     */
+    const_p_teca_dataset reduce_local(int device_id,
         std::vector<const_p_teca_dataset> local_data);
 
-    const_p_teca_dataset reduce_remote(const_p_teca_dataset local_data);
+    /** The driver for reducing the remote datasets. Calls the ::reduce override
+     * as needed.
+     */
+    const_p_teca_dataset reduce_remote(int device_id,
+        const_p_teca_dataset local_data);
 
 private:
     long start_index;
