e3a25a8c708d4e53fc9b5ab9d8dc80697186a73e
Burlen Loring <bloring@lbl.gov>
2022-05-23 09:12:14 -0700

    block time step mapper refactor
    * pass request keys in rather thna look them up
    * use standardized partitioning code

--- io/teca_cf_block_time_step_mapper.cxx
+++ io/teca_cf_block_time_step_mapper.cxx
@@ -60,22 +60,23 @@ int teca_cf_block_time_step_mapper::get_file_id(long time_step, long &file_id)
 }
 
 // --------------------------------------------------------------------------
-p_teca_cf_layout_manager teca_cf_block_time_step_mapper::get_layout_manager(
-    long time_step)
+p_teca_cf_layout_manager
+teca_cf_block_time_step_mapper::get_layout_manager(long time_step)
 {
-    long file_id = (time_step - this->start_time_step)/this->n_time_steps_per_file;
+    long file_id = (time_step - this->start_time_step) / this->n_time_steps_per_file;
     file_table_t::iterator it = this->file_table.find(file_id);
     if (it == this->file_table.end())
     {
         TECA_ERROR("No layout manager for time step " << time_step)
         return nullptr;
     }
+
     return it->second;
 }
 
 // --------------------------------------------------------------------------
 int teca_cf_block_time_step_mapper::initialize(MPI_Comm comm, long first_step,
-    long last_step, long steps_per_file, const teca_metadata &md)
+    long last_step, long steps_per_file, const std::string &request_key) 
 {
 #if !defined(TECA_HAS_MPI)
     (void)comm;
@@ -83,40 +84,9 @@ int teca_cf_block_time_step_mapper::initialize(MPI_Comm comm, long first_step,
     this->comm = comm;
     this->start_time_step = first_step;
     this->end_time_step = last_step;
+    this->n_time_steps = last_step - first_step + 1;
     this->n_time_steps_per_file = steps_per_file;
-
-    // locate the keys that enable us to know how many
-    // requests we need to make and what key to use
-    if (md.get("index_initializer_key", this->index_initializer_key))
-    {
-        TECA_ERROR("No time_step initializer key has been specified")
-        return -1;
-    }
-
-    if (md.get("index_request_key", this->index_request_key))
-    {
-        TECA_ERROR("No time_step request key has been specified")
-        return -1;
-    }
-
-    // locate available time_steps
-    this->n_time_steps = 0;
-    if (md.get(this->index_initializer_key, n_time_steps))
-    {
-        TECA_ERROR("metadata is missing the initializer key \""
-            << this->index_initializer_key << "\"")
-        return -1;
-    }
-
-    // apply restriction
-    long last
-        = this->end_time_step >= 0 ? this->end_time_step : n_time_steps - 1;
-
-    long first
-        = ((this->start_time_step >= 0) && (this->start_time_step <= last))
-            ? this->start_time_step : 0;
-
-    this->n_time_steps = last - first + 1;
+    this->index_request_key = request_key;
 
     // partition time_steps across MPI ranks. each rank
     // will end up with a unique block of time_steps
@@ -134,23 +104,14 @@ int teca_cf_block_time_step_mapper::initialize(MPI_Comm comm, long first_step,
 #endif
 
     // map time_steps to ranks
-    long n_big_blocks = this->n_time_steps%n_ranks;
+    long n_big = this->n_time_steps % n_ranks;
     this->block_size.resize(n_ranks);
     this->block_start.resize(n_ranks);
     for (int i = 0; i < n_ranks; ++i)
     {
-        this->block_size[i] = 1;
-        this->block_start[i] = 0;
-        if (i < n_big_blocks)
-        {
-            this->block_size[i] = this->n_time_steps/n_ranks + 1;
-            this->block_start[i] = first + this->block_size[i]*i;
-        }
-        else
-        {
-            this->block_size[i] = this->n_time_steps/n_ranks;
-            this->block_start[i] = first + this->block_size[i]*i + n_big_blocks;
-        }
+        // compute rank i's work assignment
+        this->block_size[i] = this->n_time_steps / n_ranks + (i < n_big ? 1 : 0);
+        this->block_start[i] = first_step + this->block_size[i]*i + (i < n_big ? 0 : n_big);
     }
 
     // get the number of files to write
@@ -164,7 +125,7 @@ int teca_cf_block_time_step_mapper::initialize(MPI_Comm comm, long first_step,
     file_ranks_i.reserve(n_ranks);
     for (long i = 0; i < this->n_files; ++i)
     {
-        long file_time_step_0 = first + i*this->n_time_steps_per_file;
+        long file_time_step_0 = first_step + i*this->n_time_steps_per_file;
         long file_time_step_1 = file_time_step_0 + this->n_time_steps_per_file - 1;
 
         file_ranks_i.clear();
@@ -205,12 +166,12 @@ int teca_cf_block_time_step_mapper::initialize(MPI_Comm comm, long first_step,
         {
             // this rank will write to this file, create a layout
             // manager that will do the work of putting data on disk
-            long first_step = i*this->n_time_steps_per_file;
+            long step = i*this->n_time_steps_per_file;
 
-            long n_steps = first_step + this->n_time_steps_per_file > this->n_time_steps ?
-                this->n_time_steps - first_step : this->n_time_steps_per_file;
+            long n_steps = step + this->n_time_steps_per_file > this->n_time_steps ?
+                this->n_time_steps - step : this->n_time_steps_per_file;
 
-            this->file_table[i] = teca_cf_layout_manager::New(comm_i, i, first + first_step, n_steps);
+            this->file_table[i] = teca_cf_layout_manager::New(comm_i, i, first_step + step, n_steps);
         }
     }
 
