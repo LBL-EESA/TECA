ff11b54d2e3afb653acf9b1c678cfaa2f2259c2c
amanda <amanda.dufek@gmail.com>
2023-03-16 12:27:45 -0700

    teca_detect_nodes: removed tempestextremes files and cleanup.

--- alg/third_party/tempestextremes/src/base/STLStringHelper.h
+++ alg/third_party/tempestextremes/src/base/STLStringHelper.h
@@ -18,9 +18,6 @@
 #define _STLSTRINGHELPER_H_
 
 #include <string>
-#include <vector>
-
-#include <cstring>
 
 ///	<summary>
 ///		This class exposes additional functionality which can be used to
@@ -34,56 +31,6 @@ private:
 STLStringHelper() { }
 
 public:
-
-///////////////////////////////////////////////////////////////////////////////
-
-inline static void ToLower(std::string &str) {
-	for(size_t i = 0; i < str.length(); i++) {
-		str[i] = tolower(str[i]);
-	}
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-inline static void ToUpper(std::string &str) {
-	for(size_t i = 0; i < str.length(); i++) {
-		str[i] = toupper(str[i]);
-	}
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-inline static bool IsIntegerIndex(const std::string &str) {
-	if (str.length() == 0) {
-		return false;
-	}
-	for(size_t i = 0; i < str.length(); i++) {
-		if ((str[i] < '0') || (str[i] > '9')) {
-			return false;
-		}
-	}
-	return true;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-inline static bool IsInteger(const std::string &str) {
-	if (str.length() == 0) {
-		return false;
-	}
-	for(size_t i = 0; i < str.length(); i++) {
-		if ((i == 0) && ((str[i] == '-') || (str[i] == '+'))) {
-			if (str.length() == 1) {
-				return false;
-			}
-			continue;
-		}
-		if ((str[i] < '0') || (str[i] > '9')) {
-			return false;
-		}
-	}
-	return true;
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -132,106 +79,6 @@ inline static bool IsFloat(const std::string &str) {
 	}
 	return fIsFloat;
 }
-
-///////////////////////////////////////////////////////////////////////////////
-
-static void RemoveWhitespaceInPlace(
-	std::string & strString
-) {
-	size_t sBegin = strString.length();
-	for (size_t s = 0; s < strString.length(); s++) {
-		if ((strString[s] != ' ') && (strString[s] != '\t')) {
-			sBegin = s;
-			break;
-		}
-	}
-	if (sBegin == strString.length()) {
-		strString = "";
-		return;
-	}
-
-	size_t sEnd = strString.length();
-	for (size_t s = sEnd-1; s > sBegin; s--) {
-		if ((strString[s] != ' ') && (strString[s] != '\t')) {
-			sEnd = s+1;
-			break;
-		}
-	}
-
-	strString = strString.substr(sBegin, sEnd - sBegin);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-static void ParseVariableList(
-	const std::string & strVariables,
-	std::vector< std::string > & vecVariableStrings,
-	const std::string & strDelimiters = std::string(" ,;")
-) {
-	int iVarBegin = 0;
-	int iVarCurrent = 0;
-
-	int nParenthesesLevel = 0;
-
-	if (strVariables.length() == 0) {
-		return;
-	}
-
-	for (;;) {
-		if ((iVarCurrent >= strVariables.length()) ||
-		    (strDelimiters.find(strVariables[iVarCurrent]) != std::string::npos)
-		) {
-			if (nParenthesesLevel == 0) {
-				_ASSERT(iVarCurrent > iVarBegin);
-				if (iVarCurrent == iVarBegin) {
-					_EXCEPTION1("Zero length variable string in \"%s\"",
-						strVariables.c_str());
-				}
-
-				vecVariableStrings.push_back(
-					strVariables.substr(iVarBegin, iVarCurrent - iVarBegin));
-
-				iVarBegin = iVarCurrent + 1;
-			}
-			if (iVarCurrent >= strVariables.length()) {
-				break;
-			}
-		}
-		if (strVariables[iVarCurrent] == '(') {
-			nParenthesesLevel++;
-		}
-		if (strVariables[iVarCurrent] == ')') {
-			nParenthesesLevel--;
-			if (nParenthesesLevel < 0) {
-				_EXCEPTIONT("Unmatched closed parenthesis in variable list");
-			}
-		}
-
-		iVarCurrent++;
-	}
-
-	if (nParenthesesLevel != 0) {
-		_EXCEPTIONT("Unmatched open parenthesis in variable list");
-	}
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-static std::string ConcatenateStringVector(
-	const std::vector< std::string > & vecStrings,
-	std::string strDelimiter
-) {
-	std::string strConcat;
-	for (int v = 0; v < vecStrings.size(); v++) {
-		strConcat += vecStrings[v];
-		if (v != vecStrings.size() - 1) {
-			strConcat += strDelimiter;
-		}
-	}
-	return strConcat;
-}
-
-///////////////////////////////////////////////////////////////////////////////
 
 };
 
