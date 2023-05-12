64ba52abdfb85b13b76878074c9407869910670a
Amanda S. Dufek <amanda.dufek@gmail.com>
2023-05-11 17:30:59 -0700

    teca_detect_nodes: updated app test

--- test/apps/test_detect_nodes_app_mcf.sh
+++ test/apps/test_detect_nodes_app_mcf.sh
@@ -2,7 +2,7 @@
 
 if [[ $# < 2 ]]
 then
-    echo "usage: test_tc_detect_app.sh [app prefix] [data root] "     \
+    echo "usage: test_detect_nodes_app_mcf.sh [app prefix] [data root] "     \
          "[mpi exec] [test cores]"
     exit -1
 fi
@@ -20,7 +20,19 @@ fi
 set -x
 
 # run the app
-${launcher} ${app_prefix}/teca_detect_nodes                           \
-    --input_file "${app_prefix}/../test/HighResMIP_TC_test.mcf"       \
-    --sea_level_pressure PSL --500mb_height Z1000 --300mb_height Z200 \
-    --surface_wind_u UBOT --surface_wind_v VBOT
+${launcher} ${app_prefix}/teca_detect_nodes                              \
+    --input_file "${app_prefix}/../test/ERA5_TC_test.mcf"                \
+    --sea_level_pressure MSL --geopotential Z                            \
+    --surface_wind_u VAR_10U --surface_wind_v VAR_10V                    \
+    --geopotential_at_surface ZS                                         \
+    --x_axis_variable longitude --y_axis_variable latitude               \
+    --max_lat 90 --min_lat -90 --max_lon 359.5 --min_lon 0 --last_step 1 \
+    --candidate_file test_detect_nodes_app_output.csv
+
+# run the diff
+#${app_prefix}/teca_table_diff                                            \
+#    "${data_root}/test_detect_nodes_app_ref.csv"                         \
+#    test_detect_nodes_app_output.csv "storm_id"
+#
+# clean up
+#rm test_detect_nodes_app_output.csv
