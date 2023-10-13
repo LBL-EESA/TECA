e5306fac22dfbf23c63519353de383d7fae91e9b
Burlen Loring <bloring@lbl.gov>
2023-10-13 10:25:05 -0700

    component_area_filter fix indent add comments

--- alg/teca_component_area_filter.cxx
+++ alg/teca_component_area_filter.cxx
@@ -361,7 +361,8 @@ const_p_teca_dataset teca_component_area_filter::execute(
         }
     }
 
-    // get threshold values
+    // get threshold values. these may be passed from a downstream algorithm
+    // as in the case of the TECA BARD
     double low_val = this->low_area_threshold;
     if (low_val == std::numeric_limits<double>::lowest()
         && request.has("low_area_threshold"))
@@ -504,20 +505,20 @@ const_p_teca_dataset teca_component_area_filter::execute(
             )
         )
 
-        // correct the size of the output
-        ids_out->resize(n_ids_out);
-        areas_out->resize(n_ids_out);
+   // correct the size of the output
+   ids_out->resize(n_ids_out);
+   areas_out->resize(n_ids_out);
 
-        // pass the updated set of component ids and their coresponding areas
-        // to the output
-        out_metadata.set(this->number_of_components_key + this->variable_postfix, n_ids_out);
-        out_metadata.set(this->component_ids_key + this->variable_postfix, ids_out);
-        out_metadata.set(this->component_area_key + this->variable_postfix, areas_out);
-        out_metadata.set("background_id" + this->variable_postfix, mask_value);
+   // pass the updated set of component ids and their coresponding areas
+   // to the output
+   out_metadata.set(this->number_of_components_key + this->variable_postfix, n_ids_out);
+   out_metadata.set(this->component_ids_key + this->variable_postfix, ids_out);
+   out_metadata.set(this->component_area_key + this->variable_postfix, areas_out);
+   out_metadata.set("background_id" + this->variable_postfix, mask_value);
 
-        // pass the threshold values used
-        out_metadata.set("low_area_threshold_km", low_val);
-        out_metadata.set("high_area_threshold_km", high_val);
+   // pass the threshold values used
+   out_metadata.set("low_area_threshold_km", low_val);
+   out_metadata.set("high_area_threshold_km", high_val);
 
     return out_mesh;
 }
